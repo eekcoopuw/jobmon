6efe6883724a63195f97512f85d0dcc26c579372
Logan Sandar <mlsandar@uw.edu>
2016-02-23 15:19:47 -0800

    add docstrings and thoughts on Tom's work.

--- job.py
+++ job.py
@@ -12,7 +12,7 @@ REQUEST_RETRIES = 3
 class ZmqHandler(Handler):
 
     def __init__(self, job):
-        Handler.__init__()
+        super().__init__()
         self.job = job
 
     def emit(self, record):
@@ -20,8 +20,19 @@ class ZmqHandler(Handler):
 
 
 class Job(object):
+    """client node job status logger. Pushes job status to server node through
+    zmq. Status is logged by server into sqlite database
 
+    Args
+        out_dir (string): file path where the server configuration is
+            stored.
+        jid (int, optional): job id of current process on SGE. If job id is not
+            specified, will attempt to use environment variable JOB_ID.
+        name (string, optional): name current process. If name is not specified
+            will attempt to use environment variable JOB_NAME.
+    """
     def __init__(self, out_dir, jid=None, name=None):
+        """set class defaults. attempt to connect and register with server."""
         if jid is None:
             self.jid = os.getenv("JOB_ID")
         else:
@@ -39,28 +50,51 @@ class Job(object):
         except CalledProcessError:
             self.job_info = None
 
+        # connect and register with server
         self.out_dir = os.path.abspath(os.path.expanduser(out_dir))
         self.connect()
         self.register()
 
     def connect(self):
+        """Connect to server. Reads config file from out_dir specified during
+        class instantiation to get socket. Client will block on send unless it
+        has successfully received a reply back from server. Not an API method,
+        needs to be underscored"""
         with open("%s/monitor_info.json" % self.out_dir) as f:
             mi = json.load(f)
-        context = zmq.Context()
-        self.socket = context.socket(zmq.REQ)
-        self.socket.setsockopt(zmq.LINGER, 0)
+        context = zmq.Context()  # default 1 i/o thread
+        self.socket = context.socket(zmq.REQ)  # blocks socket on send
+        self.socket.setsockopt(zmq.LINGER, 0)  # do not block on send.
         print 'Connecting...'
+
+        # use host and port from network filesystem cofig. option "out_dir"
         self.socket.connect(
             "tcp://{sh}:{sp}".format(sh=mi['host'], sp=mi['port']))
-        self.poller = zmq.Poller()
+
+        # setup zmq poller to poll for messages on the socket connection.
+        # we only register 1 socket but poller supports multiple.
+        self.poller = zmq.Poller()  # poll
         self.poller.register(self.socket, zmq.POLLIN)
 
     def disconnect(self):
+        """disconnect from socket and unregister with poller. Is this an API
+        method? Should be underscored if not"""
         print 'Disconnecting...'
         self.socket.close()
         self.poller.unregister(self.socket)
 
     def send_request(self, message, verbose=False):
+        """send request dictionary to server. Need to document what form this
+        dict takes.
+
+        Args:
+            message (dict): what should this look like????????????
+            verbose (bool, optional): Whether to print the servers reply to
+                stdout as well as return it. Defaults to False.
+
+        Returns:
+            Server reply message
+        """
         if isinstance(message, dict):
             message = json.dumps(message)
         reply = self.send_lazy_pirate(message)
@@ -69,14 +103,24 @@ class Job(object):
         return reply
 
     def send_lazy_pirate(self, message):
-        retries_left = REQUEST_RETRIES
+        """Safely send messages to server. This is not an api method. should
+        perhaps be underscored. Use send_request method instead
+
+        Args:
+            message (dict): what should this look like????????????
+
+        Returns:
+            Server reply message
+        """
+        retries_left = REQUEST_RETRIES  # this should be configurable
         print 'Sending message...'
         while retries_left:
-            if self.socket.closed:
+            if self.socket.closed:  # connect to socket if disconnected?
                 self.connect()
-            self.socket.send(message)
+            self.socket.send(message)  # send message to server
             expect_reply = True
             while expect_reply:
+                # ask for response from server. wait until REQUEST_TIMEOUT
                 socks = dict(self.poller.poll(REQUEST_TIMEOUT))
                 if socks.get(self.socket) == zmq.POLLIN:
                     reply = self.socket.recv()
@@ -100,6 +144,9 @@ class Job(object):
         return reply
 
     def register(self):
+        """send registration request to server. server will create database
+        entry for this job. This is not an API method so should probably be
+        masked with an underscore"""
         if self.job_info is not None:
             msg = {
                 'action': 'create_job',
@@ -116,20 +163,24 @@ class Job(object):
         self.send_request(msg)
 
     def start(self):
+        """log job start with server"""
         msg = {'action': 'update_job_status', 'args': [self.jid, 2]}
         self.send_request(msg)
 
     def failed(self):
+        """log job failure with server"""
         msg = {'action': 'update_job_status', 'args': [self.jid, 3]}
         self.send_request(msg)
 
     def log_error(self, msg):
+        """log job error with server"""
         msg = json.dumps({
             'action': 'log_error',
             'args': [self.jid, msg]})
         self.send_request(msg)
 
     def finish(self):
+        """log job complete with server"""
         msg = {'action': 'update_job_status', 'args': [self.jid, 4]}
         self.send_request(msg)
 
--- monitor.py
+++ monitor.py
@@ -17,8 +17,16 @@ Session = sessionmaker()
 
 
 class JobMonitor(object):
+    """server node job status logger.
+
+    Args:
+        out_dir (string): full filepath of directory to create job monitor
+            sqlite database in.
+    """
 
     def __init__(self, out_dir):
+        """set class defaults. make out_dir if it doesn't exist. write config
+        for client nodes to read. make sqlite database schema"""
         self.out_dir = os.path.abspath(os.path.expanduser(out_dir))
         self.out_dir = os.path.realpath(self.out_dir)
         try:
@@ -26,17 +34,24 @@ class JobMonitor(object):
         except:
             pass
         self.port, self.socket = self.start_server()
-        self.session = self.create_job_db()
+        self.session = self.create_job_db()  # don't like this
 
     def create_job_db(self):
-        dbfile = '%s/job_monitor.sqlite' % self.out_dir
+        # I think this is not the best implementation to have a create_job_db
+        # method return a session object. what happens if you want to restart
+        # the server but resume using an old database.
+        dbfile = '{out_dir}/job_monitor.sqlite'.format(out_dir=self.out_dir)
 
         def creator():
-            return sqlite3.connect('file:%s?vfs=unix-none' % dbfile, uri=True)
+            return sqlite3.connect(
+                'file:{dbfile}?vfs=unix-none'.format(dbfile=dbfile), uri=True)
         eng = sql.create_engine('sqlite://', creator=creator)
+
+        # what happens if this already exists?
         models.Base.metadata.create_all(eng)
         Session.configure(bind=eng)
         session = Session()
+
         try:
             models.default_statuses(session)
         except:
@@ -44,6 +59,7 @@ class JobMonitor(object):
         return session
 
     def create_job(self, jid, name, *args, **kwargs):
+        """create job entry in database job table"""
         job = self.session.query(models.Job).filter_by(jid=jid).first()
         if job is None:
             job = models.Job(
@@ -56,6 +72,7 @@ class JobMonitor(object):
         return job
 
     def update_job_status(self, jid, status_id):
+        """update status of job"""
         status = models.JobStatus(jid=jid, status=status_id)
         job = self.session.query(models.Job).filter_by(jid=jid).first()
         job.current_status = status_id
@@ -64,45 +81,57 @@ class JobMonitor(object):
         return status
 
     def log_error(self, jid, error):
+        """log an error"""
         error = models.JobError(jid=jid, description=error)
         self.session.add(error)
         self.session.commit()
         return error
 
     def node_name(self):
+        """return node name. perfect candidate for a property decorator"""
         return gethostname()
 
     def write_connection_info(self, host, port):
+        """dump server connection configuration to network filesystem for
+        client nodes to read"""
         with open('%s/monitor_info.json' % self.out_dir, 'w') as f:
             json.dump({'host': host, 'port': port}, f)
 
     def start_server(self):
+        """configure server and set to listen. returns tuple (port, socket).
+        doesn't actually run server despite having start in the name."""
         print('Starting server...')
         context = zmq.Context()
-        self.socket = context.socket(zmq.REP)
+        self.socket = context.socket(zmq.REP)  # server blocks on recieve
         self.port = self.socket.bind_to_random_port('tcp://*')
-        self.write_connection_info(self.node_name(), self.port)
+        self.write_connection_info(self.node_name(), self.port)  # dump config
         print('Server started.')
         return self.port, self.socket
 
     def stop_server(self):
+        """stops listening at network socket/port."""
         print('Stopping server...')
         self.socket.close()
         print('Server stopped.')
         return True
 
     def restart_server(self):
+        """restart listening at new network socket/port"""
         self.stop_server()
         self.start_server()
 
     def run(self):
+        """Run server. Start consuming registration and status updates from
+        jobs. maybe this method should have a resume option where it reads
+        a preexisting config file instead of binding to a random port every
+        time."""
         if self.socket.closed:
             print('Server offline, starting...')
             self.start_server()
         keep_alive = True
         while keep_alive:
-            msg = self.socket.recv()
-            msg = msg.decode('utf-8')
+            msg = self.socket.recv()  # server blocks on recieve
+            msg = msg.decode('utf-8')  # json is byte stream. decode to unicode
             try:
                 if msg == 'stop':
                     keep_alive = False
@@ -116,6 +145,8 @@ class JobMonitor(object):
                     else:
                         kwargs = {}
                     tocall(*msg['args'], **kwargs)
+
+                    # should send whatever tocall() returns
                     self.socket.send(b"OK")
             except Exception as e:
                 print(e)
