b666216557ccc0b35c23d9fe298cb2acd81f8b45
Logan Sandar <mlsandar@uw.edu>
2016-04-13 15:59:26 -0700

    prep for package style imports

--- jobmon/launch_monitor.py
+++ jobmon/launch_monitor.py
@@ -1,10 +1,6 @@
 # Command line interface for starting a jobmon monitor
 import sys
-import os
-this_file = os.path.abspath(os.path.expanduser(__file__))
-this_dir = os.path.dirname(os.path.realpath(this_file))
-sys.path.append(this_dir + "/..")
-from jobmon import monitor
+from .jobmon import monitor
 
 
 if __name__ == '__main__':
