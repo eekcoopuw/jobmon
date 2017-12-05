ac20eff7fd925b5d09b8b7c10546ac1bfe77a21f
Christine Pinho <cpinho@uw.edu>
2017-12-05 10:18:00 -0800

    merging abstract and executable tasks

--- tests/test_task.py
+++ tests/test_task.py
@@ -1,27 +1,26 @@
 import pytest
 
-from jobmon.workflow.abstract_task import AbstractTask
 from jobmon.workflow.executable_task import ExecutableTask
 from jobmon.workflow.bash_task import BashTask
 
 
 def test_good_names():
-    assert AbstractTask.is_valid_sge_job_name("fred")
-    assert AbstractTask.is_valid_sge_job_name("fred123")
-    assert AbstractTask.is_valid_sge_job_name("fred_and-friends")
+    assert ExecutableTask.is_valid_sge_job_name("fred")
+    assert ExecutableTask.is_valid_sge_job_name("fred123")
+    assert ExecutableTask.is_valid_sge_job_name("fred_and-friends")
 
 
 def test_bad_names():
     with pytest.raises(ValueError) as exc:
-        AbstractTask.is_valid_sge_job_name("")
+        ExecutableTask.is_valid_sge_job_name("")
     assert "None" in str(exc.value)
 
     with pytest.raises(ValueError) as exc:
-        AbstractTask.is_valid_sge_job_name("16")
+        ExecutableTask.is_valid_sge_job_name("16")
     assert "digit" in str(exc.value)
 
     with pytest.raises(ValueError) as exc:
-        AbstractTask.is_valid_sge_job_name("bad/dog")
+        ExecutableTask.is_valid_sge_job_name("bad/dog")
     assert "special" in str(exc.value)
 
 
