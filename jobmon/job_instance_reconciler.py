2c65afda7339b6d6cf61b5d9dc731db4925c3f17
Christine Pinho <cpinho@uw.edu>
2018-02-06 09:40:46 -0800

    Changing mysterioulsy disappeared message

--- jobmon/job_instance_reconciler.py
+++ jobmon/job_instance_reconciler.py
@@ -44,7 +44,8 @@ class JobInstanceReconciler(object):
             if job_instance.executor_id:
                 if job_instance.executor_id not in actual:
                     job_instance_id = job_instance.job_instance_id
-                    self._log_mysterious_error(job_instance_id)
+                    self._log_mysterious_error(job_instance_id,
+                                               job_instance.executor_id)
                     missing_job_instance_ids.append(job_instance_id)
         return missing_job_instance_ids
 
@@ -108,11 +109,16 @@ class JobInstanceReconciler(object):
             job_instances = []
         return job_instances
 
-    def _log_mysterious_error(self, job_instance_id):
+    def _log_mysterious_error(self, job_instance_id, executor_id):
         return self.jsm_req.send_request({
             'action': 'log_error',
             'kwargs': {'job_instance_id': job_instance_id,
-                       'error_message': "Job has mysteriously disappeared"}
+                       'error_message': ("Job no longer visible in qstat, "
+                                         "check qacct or jobmon database for "
+                                         "sge executor_id {} and "
+                                         "job_instance_id {}"
+                                         .format(job_instance_id, executor_id))
+                       }
         })
 
     def _log_timeout_error(self, job_instance_id):
