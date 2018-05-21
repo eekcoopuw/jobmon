6ccf2ff701ad9db7d1278b4f54734b0f8069cdbf
Christine Pinho <cpinho@uw.edu>
2018-05-21 15:23:21 -0700

    more bug fixes

--- jobmon/config.py
+++ jobmon/config.py
@@ -13,6 +13,15 @@ except ImportError:
     JSONDecodeError = ValueError
 
 
+def derive_jobmon_command_from_env():
+        singularity_img_path = os.environ.get('IMGPATH', None)
+        if singularity_img_path:
+            return (
+                'singularity run --app jobmon_command {}'
+                .format(singularity_img_path).encode())
+        return None
+
+
 class InvalidConfig(Exception):
     pass
 
@@ -59,7 +68,7 @@ class GlobalConfig(object):
         "slack_token": None,
         "default_wf_slack_channel": None,
         "default_node_slack_channel": None,
-        "jobmon_command": None
+        "jobmon_command": derive_jobmon_command_from_env()
     }
 
     def __init__(self, conn_str, jsm_host, jqs_host, jsm_rep_port,
@@ -173,18 +182,7 @@ class GlobalConfig(object):
                 gc_opts[opt] = opts_dict[opt]
             else:
                 gc_opts[opt] = GlobalConfig.default_opts[opt]
-        GlobalConfig.default_opts['jobmon_command'] = \
-            GlobalConfig.derive_jobmon_command_from_env()
         return gc_opts
-
-    @staticmethod
-    def derive_jobmon_command_from_env():
-        singularity_img_path = os.environ.get('IMGPATH', None)
-        if singularity_img_path:
-            return (
-                'singularity run --app /opt/conda/bin/jobmon_command {}'
-                .format(singularity_img_path))
-        return None
 
 
 # The config singleton... if you need to update it, modify the object directly
