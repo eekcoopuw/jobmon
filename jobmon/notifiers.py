2beb4ba175093a67eef1c7967a58ab9726824891
Christine Pinho <cpinho@uw.edu>
2018-05-29 15:36:47 -0700

    catching slack outage error

--- jobmon/notifiers.py
+++ jobmon/notifiers.py
@@ -1,4 +1,8 @@
 import requests
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 class SlackNotifier(object):
@@ -7,15 +11,21 @@ class SlackNotifier(object):
 
         self._token = token
         self.default_channel = default_channel
+        self.slack_api_url = 'https://slack.com/api/chat.postMessage'
 
     def send(self, msg, channel=None):
         if not channel:
             channel = self.default_channel
-        resp = requests.post(
-            'https://slack.com/api/chat.postMessage',
-            headers={'Authorization': 'Bearer {}'.format(self._token)},
-            json={'channel': channel, 'text': msg}
-        )
-        if resp.status_code != requests.codes.OK:
-            raise RuntimeError(
-                "Could not send Slack message. {}".format(resp.content))
+        try:
+            resp = requests.post(
+                self.slack_api_url,
+                headers={'Authorization': 'Bearer {}'.format(self._token)},
+                json={'channel': channel, 'text': msg})
+            if resp.status_code != requests.codes.OK:
+                raise RuntimeError(
+                    "Could not send Slack message. {}".format(resp.content))
+        except RuntimeError as e:
+            if "Server Error" in str(e):  # catch Slack server outage error
+                logger.error(str(e))
+            else:
+                raise
---
+++ tests/test_notifiers.py
@@ -0,0 +1,12 @@
+import pytest
+
+from jobmon.notifiers import SlackNotifier
+
+
+def test_raises_correctly():
+    notifier = SlackNotifier(token='fake_token',
+                             default_channel='jobmon-alerts')
+    notifier.slack_api_url = 'https://slack.com/apis/chat.postMessage'
+    with pytest.raises(RuntimeError):
+        notifier.send(msg="This should fail because of fake link")
+
