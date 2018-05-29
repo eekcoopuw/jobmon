e3d35e5aacea4fbe87d390bd3462e28bccf4822a
Christine Pinho <cpinho@uw.edu>
2018-05-29 13:18:14 -0700

    fixing typo. manually posts to channel

--- jobmon/notifiers.py
+++ jobmon/notifiers.py
@@ -13,7 +13,7 @@ class SlackNotifier(object):
             channel = self.default_channel
         resp = requests.post(
             'https://slack.com/api/chat.postMessage',
-            headers={'Authorization': 'Bearer '.format(self._token)},
+            headers={'Authorization': 'Bearer {}'.format(self._token)},
             json={'channel': channel, 'text': msg}
         )
         if resp.status_code != requests.codes.OK:
