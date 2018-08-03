4d61874d269c3c5fdd57b4d77517162a6f2e7534
Megan Moore <mm7148@Megans-MacBook-Pro.local>
2018-08-03 15:10:28 -0700

    trying to get docker contianers to build locally

--- runserver.py
+++ runserver.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-#import getpass
+import getpass
 import json
 import os
 import shutil
@@ -13,32 +13,24 @@ DEFAULT_NODE_SLACK_CHANNEL = 'suspicious_nodes'
 
 if __name__ == "__main__":
 
-    slack_token = input("Slack bot token: ") or None
+    slack_token = getpass.getpass("Slack bot token: ") or None
     if slack_token:
         wf_slack_channel = (
             input("Slack notification channel for reporting lost workflow "
-                  "runs ('{}'): ".format(DEFAULT_WF_SLACK_CHANNEL)) or
+                  "runs ({}): ".format(DEFAULT_WF_SLACK_CHANNEL)) or
             DEFAULT_WF_SLACK_CHANNEL)
         node_slack_channel = (
             input("Slack notification channel for reporting failing nodes "
-                  "('{}'): ".format(DEFAULT_NODE_SLACK_CHANNEL)) or
+                  "({}): ".format(DEFAULT_NODE_SLACK_CHANNEL)) or
             DEFAULT_NODE_SLACK_CHANNEL)
+        with open("jobmonrc-docker", "r") as f:
+            rcdct = json.load(f)
+            rcdct['slack_token'] = slack_token
+            rcdct['default_wf_slack_channel'] = wf_slack_channel
+            rcdct['default_node_slack_channel'] = node_slack_channel
 
-        #if it is a test do not set up the containers
-        is_test_run = input("Is this a test run (yes/no)?")
-        print(is_test_run)
-
-        if is_test_run == "no":
-            with open("jobmonrc-docker", "r") as f:
-                rcdct = json.load(f)
-                rcdct['slack_token'] = slack_token
-                rcdct['default_wf_slack_channel'] = wf_slack_channel
-                rcdct['default_node_slack_channel'] = node_slack_channel
-
-            with open("jobmonrc-docker-wsecrets", "w") as f:
-                json.dump(rcdct, f)
-        else:
-            exit()
+        with open("jobmonrc-docker-wsecrets", "w") as f:
+            json.dump(rcdct, f )
     else:
         shutil.copy("jobmonrc-docker", "jobmonrc-docker-wsecrets")
 
