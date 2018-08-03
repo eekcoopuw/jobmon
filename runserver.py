fc6120c4a72f05e60fe9cf4c4beaf9e141312b36
Megan Moore <mm7148@Megans-MacBook-Pro.local>
2018-08-03 16:06:49 -0700

    password visible, other bugs were no longer a problem

--- runserver.py
+++ runserver.py
@@ -17,14 +17,12 @@ if __name__ == "__main__":
     if slack_token:
         wf_slack_channel = (
             input("Slack notification channel for reporting lost workflow "
-                  "runs ('{}'): ".format(DEFAULT_WF_SLACK_CHANNEL)) or
+                  "runs ({}): ".format(DEFAULT_WF_SLACK_CHANNEL)) or
             DEFAULT_WF_SLACK_CHANNEL)
-        print(wf_slack_channel)
         node_slack_channel = (
             input("Slack notification channel for reporting failing nodes "
-                  "('{}'): ".format(DEFAULT_NODE_SLACK_CHANNEL)) or
+                  "({}): ".format(DEFAULT_NODE_SLACK_CHANNEL)) or
             DEFAULT_NODE_SLACK_CHANNEL)
-        print(node_slack_channel)
 
 
         with open("jobmonrc-docker", "r") as f:
