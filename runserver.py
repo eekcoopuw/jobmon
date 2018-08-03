a39ccd894ee9f49ae2e812ebd83fe8a291f9e4b2
Megan Moore <mm7148@Megans-MacBook-Pro.local>
2018-08-03 14:47:48 -0700

    tryinig to spin up containers locally to check that it works

--- runserver.py
+++ runserver.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
             with open("jobmonrc-docker-wsecrets", "w") as f:
                 json.dump(rcdct, f)
         else:
-            sys.exit()
+            exit()
     else:
         shutil.copy("jobmonrc-docker", "jobmonrc-docker-wsecrets")
 
