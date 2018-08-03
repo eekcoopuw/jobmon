35ce9920070a49caca5d5edf4827c948b7c5fdc5
Megan Moore <mm7148@Megans-MacBook-Pro.local>
2018-08-03 15:58:06 -0700

    checking second part of bug

--- runserver.py
+++ runserver.py
@@ -19,26 +19,23 @@ if __name__ == "__main__":
             input("Slack notification channel for reporting lost workflow "
                   "runs ('{}'): ".format(DEFAULT_WF_SLACK_CHANNEL)) or
             DEFAULT_WF_SLACK_CHANNEL)
+        print(wf_slack_channel)
         node_slack_channel = (
             input("Slack notification channel for reporting failing nodes "
                   "('{}'): ".format(DEFAULT_NODE_SLACK_CHANNEL)) or
             DEFAULT_NODE_SLACK_CHANNEL)
+        print(node_slack_channel)
 
-        #if it is a test do not set up the containers
-        is_test_run = input("Is this a test run (yes/no)?")
-        print(is_test_run)
 
-        if is_test_run == "no":
-            with open("jobmonrc-docker", "r") as f:
-                rcdct = json.load(f)
-                rcdct['slack_token'] = slack_token
-                rcdct['default_wf_slack_channel'] = wf_slack_channel
-                rcdct['default_node_slack_channel'] = node_slack_channel
+        with open("jobmonrc-docker", "r") as f:
+            rcdct = json.load(f)
+            rcdct['slack_token'] = slack_token
+            rcdct['default_wf_slack_channel'] = wf_slack_channel
+            rcdct['default_node_slack_channel'] = node_slack_channel
 
-            with open("jobmonrc-docker-wsecrets", "w") as f:
-                json.dump(rcdct, f)
-        else:
-            exit()
+        with open("jobmonrc-docker-wsecrets", "w") as f:
+            json.dump(rcdct, f)
+
     else:
         shutil.copy("jobmonrc-docker", "jobmonrc-docker-wsecrets")
 
