559daa14873d041117d8623b4d4e2c9e089c9749
Christine Pinho <cpinho@uw.edu>
2018-08-14 14:29:21 -0700

    adding app.py

---
+++ jobmon/server/services/job_state_manager/app.py
@@ -0,0 +1,16 @@
+import os
+
+from flask import Flask
+from jobmon.server.services.job_state_manager.job_state_manager import jsm
+
+
+def create_app(host, conn_str):
+    app = Flask(__name__)
+    if host:
+        os.environ['host'] = host
+    if conn_str:
+        os.environ['conn_str'] = conn_str
+
+    app.register_blueprint(jsm)
+
+    return app
--- tests/run_services.py
+++ tests/run_services.py
@@ -1,15 +1,7 @@
 import os
-import sys
-from signal import signal, SIGTERM
 # NOTE: imports of config and the specific services are INTENTIONALLY put
 # inside these functions, because module level imports cause config issues
 # related to a load order bug. These issues will be sorted out with GBDSCI-1089
-
-
-def shutdown_session(exception=None):
-    from jobmon.server.database import ScopedSession
-    ScopedSession.remove()
-    sys.exit(0)
 
 
 def run_jsm():
@@ -19,7 +11,6 @@ def run_jsm():
     app = create_app(os.environ['host'], os.environ['conn_str'])
     app.run(host="0.0.0.0", port=get_the_client_config().jsm_port, debug=True,
             use_reloader=False, use_evalex=False, threaded=False)
-    signal(SIGTERM, shutdown_session)
 
 
 def run_jqs():
@@ -29,4 +20,3 @@ def run_jqs():
     app = create_app(os.environ['host'], os.environ['conn_str'])
     app.run(host="0.0.0.0", port=get_the_client_config().jqs_port, debug=True,
             use_reloader=False, use_evalex=False, threaded=False)
-    signal(SIGTERM, shutdown_session)
