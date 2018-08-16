4849801b1ef3f59636578ab6e41e032ce156942c
Christine Pinho <cpinho@uw.edu>
2018-08-16 09:45:09 -0700

    putting in defaults for production

--- jobmon/client/the_client_config.py
+++ jobmon/client/the_client_config.py
@@ -7,7 +7,7 @@ import jobmon
 def get_the_client_config():
     return ClientConfig(
         jobmon_version=str(jobmon.__version__),
-        host=os.environ['host'],
+        host=os.environ.get('host', 'jobmon-p01.ihme.washington.edu'),
         jsm_port=5056,
         jqs_port=5058,
         jobmon_command=derive_jobmon_command_from_env())
--- jobmon/server/the_server_config.py
+++ jobmon/server/the_server_config.py
@@ -7,7 +7,9 @@ import jobmon
 def get_the_server_config():
     return ServerConfig(
         jobmon_version=str(jobmon.__version__),
-        conn_str=os.environ['conn_str'],
+        conn_str=os.environ.get(
+            'conn_str', ("mysql://docker:docker@"
+                         "jobmon-p01.ihme.washington.edu:3313/docker")),
         slack_token=None,
         default_wf_slack_channel=None,
         default_node_slack_channel=None,
