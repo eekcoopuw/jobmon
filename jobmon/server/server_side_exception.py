1695bb852ed3e2eea3296743401c158dd3e74607
Geoffrey Phipps <gphipps@uw.edu>
2019-03-01 15:13:57 -0800

    More logging for invalid state transition

---
+++ jobmon/server/server_side_exception.py
@@ -0,0 +1,25 @@
+from logging import Logger
+import sys
+import warnings
+
+
+class ServerSideException(Exception):
+    """Used for all exceptions on the server sedie (JQS, JSM)"""
+    def __init__(self, msg: str):
+        self.msg = msg
+
+    def __str__(self):
+        return repr(self.msg)
+
+
+def log_and_raise(msg: str, logger: Logger):
+    """
+    Something bad happened, send the message to warnings, logger and
+    re-raise as a wrapped exception. A useful place to add global error
+    handling without boiler-plate code
+    """
+    warnings.warn(msg)
+    logger.error(msg)
+    # Use value traceback so that the original exception is not lost
+    (_, value, traceback) = sys.exc_info()
+    raise ServerSideException(f"{msg} from {value}").with_traceback(traceback)
