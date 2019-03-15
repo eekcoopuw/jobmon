ac978cff9799b3bf74e313e3567960d687c4b89a
limingxu <limingxu@uw.edu>
2019-03-15 16:05:55 -0700

    Tested with a local docker container

--- jobmon/server/jobmonLogging.py
+++ jobmon/server/jobmonLogging.py
@@ -1,77 +1,76 @@
 import logging
-import sys
+import socket
+
+from logging.handlers import SysLogHandler
 
 
-# this is a pointer to the module object instance itself.
-this = sys.modules[__name__]
-
-this.stream_handler = None
-this.format = '%(asctime)s [%(name)-12s] %(levelname)-8s %(message)s'
-this.level = logging.INFO
-
-
-def init_handler():
-    this.stream_handler = logging.StreamHandler()
-    formatter = logging.Formatter(this.format)
-    this.stream_handler.setFormatter(formatter)
-
-
-def getLogLevel() -> str:
-    return logging.getLevelName(this.level)
-
-
-def setLogLevel(l: int):
-    this.level = l
-
-
-def getLogger(name: str):
-    # Return the logger. If the handler is not created, create it and attach to the logger
-    logger = logging.getLogger(name)
-    if this.stream_handler is None:
-        init_handler()
-    logger.addHandler(this.stream_handler)
-    logger.setLevel(this.level)
-    return logger
-
-"""
 class jobmonLogging():
-    _logger = None
-    _handler = None
-    _format = '%(asctime)s [%(name)-12s] %(levelname)-8s %(message)s'
-    _logLevel = logging.DEBUG
+    # Constants
+    CRITICAL: int = logging.CRITICAL
+    ERROR: int = logging.ERROR
+    WARNING: int = logging.WARNING
+    INFO: int = logging.INFO
+    DEBUG: int = logging.DEBUG
+
+
+    _logger: logging.Logger = None
+    _handler: logging.Handler = None
+    _format: str = '%(asctime)s [%(name)-12s] %(levelname)-8s %(message)s'
+    _logLevel: int = INFO
 
     def __init__(self):
         # Add a standard format when the logger is called the first time
         if jobmonLogging._logger is None:
             jobmonLogging._logger = logging.getLogger("jobmonServer")
-            #This should output the logs when using docker logs
+            # This should output the logs when using docker logs
             jobmonLogging._handler = logging.StreamHandler()
             formatter = logging.Formatter(jobmonLogging._format)
             jobmonLogging._handler.setFormatter(formatter)
-            jobmonLogging.setlogLevel(jobmonLogging._logLevel)
-            jobmonLogging._logger.addHandler(jobmonLogging._handler)
             jobmonLogging._logger.setLevel(jobmonLogging._logLevel)
+            jobmonLogging._logger.addHandler(jobmonLogging._handler)
+            jobmonLogging._handler.setLevel(jobmonLogging._logLevel)
 
     @staticmethod
-    def setlogLevel(level):
+    def setlogLevel(level: int):
+        if jobmonLogging._logLevel is None:
+            jobmonLogging()
         jobmonLogging._logLevel = level
         jobmonLogging._handler.setLevel(level)
-        jobmonLogging._logger.addHandler(jobmonLogging._handler)
         jobmonLogging._logger.setLevel(level)
 
     @staticmethod
-    def getlogLevel():
+    def getlogLevel() -> int:
         return jobmonLogging._logLevel
 
     @staticmethod
-    def getLogger():
+    def getLogger(name: str = __file__) -> logging.Logger:
         if jobmonLogging._logger is None:
             jobmonLogging()
+        jobmonLogging._logger.info("----{}----".format(name))
         return jobmonLogging._logger
-"""
 
+    @staticmethod
+    def attachHandler(h: logging.Handler, l: int = logging.DEBUG):
+        jobmonLogging._logger.addHandler(h)
+        h.setLevel(l)
+        # The logger log lever needs to be set to the lowest of its handlers
+        if l < jobmonLogging._logLevel:
+            jobmonLogging.setlogLevel(l)
+
+    @staticmethod
+    def attachLocalSyslog(log_dir: str = "/dev/log", l: int = logging.DEBUG):
+        h = SysLogHandler(address=log_dir)
+        jobmonLogging.attachHandler(h, l)
+
+    @staticmethod
+    def attachRemoteSyslog(host: str, port: int, socktype=socket.SOCK_DGRAM, l: int = logging.DEBUG):
+        h = SysLogHandler(address=(host, port), socktype=socktype)
+        jobmonLogging.attachHandler(h, l)
+
+
+# The following code is for testing purpose only.
 def main():
-    """
+
     logger = jobmonLogging.getLogger()
     print(logging.getLevelName(jobmonLogging.getlogLevel()))
     logger.info("info")
@@ -79,7 +78,7 @@ def main():
     logger.warning("Warning")
     print()
 
-    jobmonLogging.setlogLevel(logging.INFO)
+    jobmonLogging.setlogLevel(logging.DEBUG)
     print(logging.getLevelName(jobmonLogging.getlogLevel()))
     logger.info("info")
     logger.debug("Debug")
@@ -91,19 +90,6 @@ def main():
     logger.info("info")
     logger.debug("Debug")
     logger.warning("Warning")
-    print()
-    """
-    logger = getLogger("lalala")
-    logger.debug(getLogLevel())
-    logger.debug("Debug")
-    logger.info("Info")
-    print()
-
-    setLogLevel(logging.DEBUG)
-    logger = getLogger("mimimi")
-    logger.debug(getLogLevel())
-    logger.debug("Debug")
-    logger.info("Info")
     print()
 
 
