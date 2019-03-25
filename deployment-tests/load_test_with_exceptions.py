b9a0f5d074f523031c038ce38a860ef1869a60ff
Megan Moore <mm7148@uw.edu>
2019-03-25 16:01:46 -0600

    set to make jobs fail at a certain rate so that retries occur and jotries occure and jobs get stuck waiting

---
+++ deployment-tests/load_test_with_exceptions.py
@@ -0,0 +1,119 @@
+import getpass
+import random
+import sys
+import uuid
+from datetime import datetime
+
+
+from jobmon.client.swarm.workflow.workflow import Workflow
+from jobmon.client.swarm.executors import sge_utils as sge
+from mock_sleep_and_write_task import SleepAndWriteFileMockTask
+
+
+def load_test_with_exceptions(n_jobs: int, n_ex: int, all_phases: bool=False)\
+        -> None:
+    """
+    :param n_jobs: number of jobs to be created (*5 if running all phases)
+    :param n_ex: number of exceptions per round of jobs
+    (*5 if running all phases)
+    :param all_phases: if you want to test a full three phase workflow or
+    just one phase to test when all jobs are instantiated at once
+    """
+    wfid = uuid.uuid4()
+    user = getpass.getuser()
+    wf = Workflow(f"load-test_{wfid}", "load_test_with_exceptions",
+                  stderr=f"/ihme/scratch/users/{user}/tests/load_test/{wfid}",
+                  stdout=f"/ihme/scratch/users/{user}/tests/load_test/{wfid}",
+                  project="proj_burdenator")
+
+    tier1 =[]
+    for i in range(n_jobs):
+        sleep = random.randint(30, 41)
+        uid = f"tier1_{uuid.uuid4()}"
+        cs = sge.true_path(f"deployment-tests/sleep_and_err.py --unique_id "
+                           f"{uid}")
+        task = SleepAndWriteFileMockTask(
+            command=f"python {cs} --sleep_secs {sleep}")
+        tier1.append(task)
+
+    num_tasks = n_jobs
+
+    if all_phases:
+        num_tasks = 5*n_jobs
+        tier2 = []
+        # Second Tier, depend on 1 tier 1 task
+        for i in range(n_jobs * 3):
+            sleep = random.randint(30, 41)
+            uid = f"tier2_{uuid.uuid4()}"
+            cs = sge.true_path(f"deployment-tests/sleep_and_err.py --unique_id"
+                               f" {uid}")
+            task=SleepAndWriteFileMockTask(command=f"python {cs} --sleep_secs"
+                                                   f" {sleep}",
+                                           upstream_tasks=[tier1[(i%n_jobs)]])
+            tier2.append(task)
+        # if you run all tasks all first tier tasks will run smoothly, then
+        # tier 2 tasks will start, and some will error out which will cause
+        # some tier 3 tasks to time out
+        add_random_err(tier2, (n_ex*3))
+
+        tier3 = []
+        # Third Tier, depend on 3 tier 2 tasks
+        for i in range(n_jobs):
+            sleep = random.randint(30, 41)
+            uid = f"tier3_{uuid.uuid4()}"
+            cs = sge.true_path(f"deployment-tests/sleep_and_err.py --unique_id"
+                               f" {uid}")
+            task =SleepAndWriteFileMockTask(command=f"python {cs} --sleep_secs"
+                                                    f" {sleep}",
+                                            upstream_tasks=
+                                            [tier2[i], tier2[(i+n_jobs)],
+                                             tier2[(i+(2*n_jobs))]])
+            tier3.append(task)
+
+        add_random_err(tier3, n_ex)
+        wf.add_tasks(tier1+tier2 + tier3)
+
+    else:
+        add_random_err(tier1, n_ex)
+        wf.add_tasks(tier1)
+
+    time = datetime.now().strftime("%m/%d/%Y_%H:%M:%S")
+    print(f"{time}: Beginning the workflow, there are {num_tasks} tasks in "
+          f"this DAG")
+    wf.execute()
+    time = datetime.now().strftime("%m/%d/%Y/_%H:%M:%S")
+    print(f"{time}: Workflow complete!")
+
+
+def add_random_err(task_list, n_ex):
+    """set a random group of tasks to fail seeded with 4 so we can replicate
+    the random group """
+    random.seed(4)
+    sample = random.sample(task_list, n_ex)
+    for task in sample:
+        task.command += " --fail_always"
+
+
+if __name__ == "__main__":
+    """
+    ex. call ' python deployment-tests/load_test_with_exceptions.py 1 0 
+    --all_phases'
+     to run three tiers 1 job, 3 jobs, 1 job, without any raising exceptions
+    """
+    n_jobs = 1
+    n_ex = 0 #default set to not have any jobs that error out
+    all_phases = False
+    print(len(sys.argv))
+    if len(sys.argv) > 1:
+        n_jobs = int(sys.argv[1])
+        n_ex = int(sys.argv[2])
+        if len(sys.argv) > 3:
+            if sys.argv[3] == '--all_phases':
+                all_phases = True
+        assert n_jobs > 0, "Please provide an integer greater than 0 for the" \
+                           " number of jobs"
+        assert (0 <= n_ex < n_jobs), "Please provide a value for the"\
+                                     " number of jobs that will error"\
+                                     " out that is less than the " \
+                                     "number of jobs and not negative"
+    load_test_with_exceptions(n_jobs, n_ex, all_phases=all_phases)
---
+++ deployment-tests/sleep_and_err.py
@@ -0,0 +1,76 @@
+import argparse
+import os
+import time
+
+from getpass import getuser
+
+
+def main():
+    """
+    The remote script that MockSleepAndWrite calls
+    """
+
+    parser = argparse.ArgumentParser(description='mock job')
+    parser.add_argument('--sleep_secs',
+                        type=int, default=10,
+                        action='store',
+                        help='The number of seconds to sleep before writing '
+                             'or dying')
+    parser.add_argument('--fail_always',
+                        action='store_true', default=False,
+                        help='If true, sleep and then raise a ValueError')
+    parser.add_argument('--fail_count',
+                        type=int, default=0,
+                        action='store',
+                        help='If true, then check the a file for the count of '
+                             'previous failures (stateful!).'
+                             'If previous fails < fail_count then fail')
+    parser.add_argument('--unique_id',
+                        type=str, default='uuid',
+                        action='store',
+                        help='If set, commands will be made unique so that '
+                             'you can test the same command (sleep) in many '
+                             'iterations without getting warnings')
+    parser.add_argument('--fail_count_fp',
+                        type=str,
+                        default=f'/ihme/scratch/users/{getuser()}/tests/'
+                                f'fail_count',
+                        action='store',
+                        help='filepath to count how many failures have '
+                               'occurred')
+    args = parser.parse_args()
+
+    if args.fail_always:
+        time.sleep(args.sleep_secs)
+        raise ValueError("Mock task failing permanently by command line arg")
+    elif args.fail_count:
+        # Go check how many times this script has failed
+        counter_file = "{}-count".format(args.fail_count_fp)
+        if os.path.exists(counter_file):
+            # Not the first time, let's see how many times we have failed
+            fp = open(counter_file, "r")
+            count_so_far = int(fp.read())
+            fp.close()
+            os.remove(counter_file)
+        else:
+            # First time, have not yet failed
+            count_so_far = 0
+
+        if count_so_far < args.fail_count:
+            # Have not yet failed enough
+            count_so_far += 1
+            fp = open(counter_file, "w")
+            fp.write("{}\n".format(count_so_far))
+            fp.close()
+            raise ValueError("Programmed intermittent failures, total "
+                             "failures so far: {}".format(count_so_far))
+        else:
+            # Enough failures, we should succeed this time
+            time.sleep(args.sleep_secs)
+    else:
+        # No "fail" argument, we should succeed
+        time.sleep(args.sleep_secs)
+
+
+if __name__ == "__main__":
+    main()
