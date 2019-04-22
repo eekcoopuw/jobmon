9c2cd500569aa2e79512519b10a6183dc50d9020
limingxu <limingxu@uw.edu>
2019-04-22 10:57:11 -0700

    I do not see logic issue with publishing here; thus remove the todo.

--- jobmon/server/job_state_manager/job_state_manager.py
+++ jobmon/server/job_state_manager/job_state_manager.py
@@ -744,11 +744,6 @@ def _update_job_instance_state(job_instance, status_id):
 
     job = job_instance.job
 
-    # TODO: Investigate moving this publish logic into some SQLAlchemy-
-    # event driven framework. Given the amount of code copying here, to
-    # ensure consistency with committed transactions it doesn't feel like
-    # the JobStateManager should be the responsible party on this one.
-    #
     # ... see tests/tests_job_state_manager.py for Event example
     if job.status in [JobStatus.DONE, JobStatus.ERROR_FATAL]:
         to_publish = mogrify(job.dag_id, (job.job_id, job.status))
