2d5b26615340b74ed2622cb3d64f5163946efa27
Geoffrey Phipps <gphipps@uw.edu>
2019-05-21 12:33:39 -0700

    Remove stray character

--- tests/test_job_state_manager.py
+++ tests/test_job_state_manager.py
@@ -101,7 +101,7 @@ def test_get_workflow_run_id(db_cfg, real_dag_id):
                  'slack_channel': "",
                  'executor_class': 'SGEExecutor',
                  'working_dir': "",
-+                'resource_adjustment': "0.5"},
+                 'resource_adjustment': "0.5"},
         request_type='post')
     wf_run_id = response['workflow_run_id']
     # make sure that the wf run that was just created matches the one that
