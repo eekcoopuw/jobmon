97064871683a718b542bd3a903e4b860068389fb
Megan Moore <mm7148@gen-uge-archive-p166.cluster.ihme.washington.edu>
2019-06-12 14:54:25 -0700

    load test to replicate workflows of tasks that create new workflows

--- deployment_tests/multi_workflow_test.py
+++ deployment_tests/multi_workflow_test.py
@@ -1,15 +1,10 @@
 import getpass
 import os
-import random
 import stat
 import sys
 import uuid
-from datetime import datetime
 
 from jobmon.client.swarm.workflow.workflow import Workflow
-
-from jobmon import BashTask
-
 from jobmon import PythonTask
 
 thisdir = os.path.dirname(os.path.realpath(os.path.expanduser(__file__)))
@@ -17,10 +12,12 @@ thisdir = os.path.dirname(os.path.realpath(os.path.expanduser(__file__)))
 
 def multi_workflow_test(n_workflows: int) -> None:
     """
-    Creates and runs a workflow that then creates and runs n workflows
-    that each run 3 sleeping jobs. This will test the capacity for jobmon to
+    Creates and runs a workflow that creates and runs n workflows that each
+    run 3 sleeping jobs. This will test the capacity for jobmon services to
     handle the requests that come from a large number of reconciler and job
     instance factory instances
+
+    ex command to replicate codem behavior: python multi_workflow_test.py 700
     """
 
     wfid = uuid.uuid4()
@@ -29,32 +26,20 @@ def multi_workflow_test(n_workflows: int) -> None:
     st = os.stat(command)
     os.chmod(command, st.st_mode | stat.S_IXUSR)
 
+    script = os.path.join(thisdir, "create_worker_workflow.py")
+    st = os.stat(script)
+    os.chmod(script, st.st_mode | stat.S_IXUSR)
+
     master_wf = Workflow(f"master_workflow_{wfid}",
                          name="master_workflow",
                          stderr=f"/ihme/scratch/users/{user}/tests/load_test/{wfid}",
                          stdout=f"/ihme/scratch/users/{user}/tests/load_test/{wfid}",
                          project="proj_tools")
-    for i in n_workflows:
-        task = PythonTask(script=f"create_worker_workflow({wfid}, {user}, "
-                          f"{command}", name=f"worker_{i}", num_cores=2,
-                          m_mem_free='1G')
+    for i in range(n_workflows):
+        task = PythonTask(script=f"{script} {i} {wfid}",
+                          name=f"worker_{i}", num_cores=2, m_mem_free='1G')
         master_wf.add_task(task)
     master_wf.run()
-
-
-def create_worker_workflow(wfid, user, command) -> None:
-    uid = uuid.uuid4()
-    worker_wf = Workflow(f"worker_wf_{wfid}_{i}",
-                         name=f"worker_workflow_{i}",
-                         stderr=f"/ihme/scratch/users/{user}/tests/load_test/{wfid}_{i}",
-                         stdout=f"/ihme/scratch/users/{user}/tests/load_test/{wfid}_{i}",
-                         project="proj_tools")
-    task_1 = BashTask(f"{command} 10 {uid}", slots=1)
-    task_2 = BashTask(f"{command} 15 {uid}", slots=1)
-    task_3 = BashTask(f"{command} 20 {uid}", slots=1)
-
-    worker_wf.add_tasks([task_1, task_2, task_3])
-    worker_wf.run()
 
 
 if __name__ == "__main__":
