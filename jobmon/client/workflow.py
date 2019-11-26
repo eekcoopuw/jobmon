720bf69e5330d2f31195ba5f60088327be92178b
Logan Sandar <mlsandar@uw.edu>
2019-11-26 10:22:27 -0600

    commit before pulling megan

--- jobmon/client/workflow.py
+++ jobmon/client/workflow.py
@@ -1,10 +1,14 @@
 from collections import OrderedDict
-from typing import Dict
+from http import HTTPStatus as StatusCodes
+from typing import Dict, Optional
 import uuid
 
 from jobmon.client import shared_requester
 from jobmon.client._logging import ClientLogging as logging
+from jobmon.client.dag import Dag
 from jobmon.client.requests.requester import Requester
+from jobmon.client.task import Task
+from jobmon.client.swarm.workflow.bound_task import BoundTask
 
 
 logger = logging.getLogger(__name__)
@@ -79,13 +83,12 @@ class Workflow(object):
         self.workflow_args = workflow_args
         self.name = name
         self.description = description
+        self.status = "G"
 
         self.requester = requester
 
         # hash to task object mapping
         self.tasks: OrderedDict = OrderedDict()
-        # hash to bound task object mapping
-        self.bound_tasks: Dict[int, BoundTask] = {}
 
         if workflow_args:
             self.workflow_args = workflow_args
@@ -99,9 +102,102 @@ class Workflow(object):
                         "Then add the same tasks to this workflow"
                         .format(self.workflow_args))
 
+    @property
+    def workflow_id(self) -> int:
+        if not hasattr(self, "_workflow_id"):
+            raise AttributeError(
+                "workflow_id cannot be accessed before workflow is bound")
+        return self._workflow_id
+
+    @property
+    def dag_id(self) -> int:
+        if not hasattr(self, "_workflow_id"):
+            raise AttributeError(
+                "dag_id cannot be accessed before workflow is bound")
+        return self._node.dag_id
+
     def run(self,
             fail_fast: bool = False,
             seconds_until_timeout: int = 36000,
             resume: bool = ResumeStatus.DONT_RESUME,
             reset_running_jobs: bool = True):
+        pass
+
+    def add_task(self, task: Task):
+        """Add a task to the workflow to be executed.
+           Set semantics - add tasks once only, based on hash name. Also
+           creates the job. If is_no has no task_id the creates task_id and
+           writes it onto object.
+        """
+        logger.debug(f"Adding Task {task}")
+        if task.hash in self.tasks:
+            raise ValueError(f"A task with hash {task.hash} already exists. "
+                             f"All tasks in a workflow must have unique "
+                             f"commands. Your command was: {task.command}")
+        self.tasks[hash(task)] = task
+        logger.debug(f"Task {task.hash} added")
+
+        return task
+
+    def add_tasks(self, tasks: Task):
+        """Add a list of task to the workflow to be executed"""
+        for task in tasks:
+            self.add_task(task)
+
+    def _create_bound_task(self, task: Task):
+        bound_task = BoundTask()
+        return bound_task
+
+    def bind(self):
+        # create DAG
+        dag = Dag()
+
+        # bind nodes and add to DAG
+        for _, task in self.tasks.values():
+            node = task.node
+            node.bind()
+            dag.add_node(task.node)
+
+        # bind DAG/add edges to db
+        dag.bind()
+
+        # get workflow_id
+        workflow_id = self._get_workflow_id()
+        if workflow_id is None:
+            workflow_id = self._add_workflow()
+        self._workflow_id = workflow_id
+
+        # add tasks to workflow
+        for _, task in self.tasks.values():
+            task.workflow_id = self.workflow_id
+            task.bind()
+
+    def _get_workflow_id(self) -> Optional[int]:
+        return_code, response = self.requester.send_request(
+            app_route='/workflow',
+            message={
+            },
+            request_type='get'
+        )
+        if return_code != StatusCodes.OK:
+            raise ValueError(f'Unexpected status code {return_code} from GET '
+                             f'request through route /workflow. Expected code '
+                             f'200. Response content: {response}')
+        return response['workflow_id']
+
+    def _add_workflow(self) -> int:
+        app_route = f'/workflow'
+        return_code, response = self.requester.send_request(
+            app_route=app_route,
+            message={
+            },
+            request_type='post'
+        )
+        if return_code != StatusCodes.OK:
+            raise ValueError(f'Unexpected status code {return_code} from PUT '
+                             f'request through route {app_route}. Expected '
+                             f'code 200. Response content: {response}')
+        return response["workflow_id"]
+
+    def __hash__(self) -> int:
         pass
