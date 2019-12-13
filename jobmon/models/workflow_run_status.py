3df109b6673f2f354d9161f8b227fcde8fa7be75
Naris Silpakit <nariss@uw.edu>
2019-12-12 17:27:42 -0800

    remove schema change, replace CR status with C and HR status with H

--- jobmon/models/workflow_run_status.py
+++ jobmon/models/workflow_run_status.py
@@ -13,8 +13,8 @@ class WorkflowRunStatus(DB.Model):
     STOPPED = 'S'
     ERROR = 'E'
     DONE = 'D'
-    COLD_RESUME = 'CR'
-    HOT_RESUME = 'HR'
+    COLD_RESUME = 'C'
+    HOT_RESUME = 'H'
 
     id = DB.Column(DB.String(1), primary_key=True)
     label = DB.Column(DB.String(150), nullable=False)
