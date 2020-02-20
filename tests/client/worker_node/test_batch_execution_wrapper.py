66ba1ff52b8833f48ce3b07b977dfa57d0a07f7a
limingxu <limingxu@uw.edu>
2020-02-20 11:51:37 -0800

    It seems to me something is not right with PythonTask. Change to BashTask.

---
+++ tests/client/worker_node/test_batch_execution_wrapper.py
@@ -0,0 +1,73 @@
+from getpass import getuser
+import os
+from time import sleep
+
+from jobmon.client.templates.unknown_workflow import UnknownWorkflow as Workflow
+from jobmon.client.templates.bash_task import BashTask
+
+
+thisdir = os.path.dirname(os.path.realpath(os.path.expanduser(__file__)))
+
+
+def test_sge_cli(db_cfg, client_env):
+    job_name = "foo"
+    log_dir = f'/ihme/scratch/users/{getuser()}'
+    t1 = BashTask("python fill_pipi.py",
+                  executor_class="SequentialExecutor",
+                  name=job_name,
+                  num_cores=1,
+                  max_runtime_seconds=600,
+                  max_attempts=1)
+
+    workflow = Workflow("simple_workflow2", project='proj_scicomp',
+                        stderr=log_dir, stdout=log_dir)
+    workflow.add_tasks([t1])
+    workflow.run()
+
+    # check db
+    app = db_cfg["app"]
+    DB = db_cfg["DB"]
+    with app.app_context():
+        import pdb
+        pdb.set_trace()
+        query = """
+        SELECT description
+        FROM task_instance_error_log"""
+        res = DB.session.execute(query).fetchone()
+        DB.session.commit()
+
+    error = res[0]
+    assert error == (("a" * 2**10 + "\n") * (2**8))[-10000:]
+
+    # check logs
+    with app.app_context():
+        query = """
+        SELECT executor_id
+        FROM task_instance"""
+        executor_id = DB.session.execute(query).fetchone()[0]
+        DB.session.commit()
+
+    # check stderr
+    # Be careful, it can take a little while to appear
+        stderr_name = os.path.join(log_dir, f"{job_name}.e{executor_id}")
+    wait_for_file(stderr_name)
+    with open(stderr_name, "r") as f:
+        content = f.read()
+    assert ("a" * 2**10 + "\n") * (2**8) in content
+
+    # check stdout
+    stdout_name = os.path.join(log_dir, f"{job_name}.o{executor_id}")
+    wait_for_file(stdout_name)
+    with open(stdout_name, "r") as f:
+        content = f.read()
+    assert ("a" * 2 ** 10 + "\n") * (2 ** 8) in content
+
+
+def wait_for_file(filepath: str) -> bool:
+    """Waits a few times to see if it appears, asserts if it takes too long"""
+    num_tries = 0
+    while not os.path.exists(filepath):
+        sleep(5)
+        num_tries += 1
+        assert num_tries < 8
+    return True
