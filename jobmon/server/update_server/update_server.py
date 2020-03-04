be6dba54e0eb8225dd31d8828208848e9cae2ba7
Logan Sandar <mlsandar@uw.edu>
2020-03-04 15:24:59 -0800

    fix bug in update server

--- jobmon/server/update_server/update_server.py
+++ jobmon/server/update_server/update_server.py
@@ -848,27 +848,27 @@ def log_wfr_heartbeat(workflow_run_id: int):
 
 
 def _transform_mem_to_gb(mem_str: str) -> float:
-   # we allow both upper and lowercase g, m, t options
-   # BUG g and G are not the same
-   if mem_str[-1].lower() == "m":
-       mem = float(mem_str[:-1])
-       mem /= 1000
-   elif mem_str[-2:].lower() == "mb":
-       mem = float(mem_str[:-2])
-       mem /= 1000
-   elif mem_str[-1].lower() == "t":
-       mem = float(mem_str[:-1])
-       mem *= 1000
-   elif mem_str[-2:].lower() == "tb":
-       mem = float(mem_str[:-2])
-       mem *= 1000
-   elif mem_str[-1].lower() == "g":
-       mem = float(mem_str[:-1])
-   elif mem_str[-2:].lower() == "gb":
-       mem = float(mem_str[:-2])
-   else:
-       mem = 1
-       return mem
+    # we allow both upper and lowercase g, m, t options
+    # BUG g and G are not the same
+    if mem_str[-1].lower() == "m":
+        mem = float(mem_str[:-1])
+        mem /= 1000
+    elif mem_str[-2:].lower() == "mb":
+        mem = float(mem_str[:-2])
+        mem /= 1000
+    elif mem_str[-1].lower() == "t":
+        mem = float(mem_str[:-1])
+        mem *= 1000
+    elif mem_str[-2:].lower() == "tb":
+        mem = float(mem_str[:-2])
+        mem *= 1000
+    elif mem_str[-1].lower() == "g":
+        mem = float(mem_str[:-1])
+    elif mem_str[-2:].lower() == "gb":
+        mem = float(mem_str[:-2])
+    else:
+        mem = 1
+    return mem
 
 
 @jsm.route('/task/<task_id>/update_resources', methods=['POST'])
@@ -895,6 +895,10 @@ def update_task_resources(task_id: int):
     logger.debug(logging.logParameter("task_id", task_id))
 
     data = request.get_json()
+    if data.get("m_mem_free") is None:
+        m_mem_free = "2g"
+    else:
+        m_mem_free = data.get("m_mem_free")
     parameter_set_type = data["parameter_set_type"]
 
     try:
@@ -911,7 +915,7 @@ def update_task_resources(task_id: int):
         context_args=data.get('context_args', None),
         queue=data.get('queue', None),
         num_cores=data.get('num_cores', None),
-        m_mem_free=_transform_mem_to_gb(data.get("m_mem_free", 2)),
+        m_mem_free=_transform_mem_to_gb(m_mem_free),
         j_resource=data.get('j_resource', False),
         resource_scales=data.get('resource_scales', None),
         hard_limits=data.get('hard_limits', False))
