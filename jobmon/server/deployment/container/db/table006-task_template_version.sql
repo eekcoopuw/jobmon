6519d6578e48f6692d19edc874e54ea47e8bf1c2
Logan Sandar <mlsandar@uw.edu>
2020-06-18 17:27:41 +0000

    Merge pull request #597 in CC/jobmon from ~MLSANDAR/jobmon:release/guppy to release/guppy
    Squashed commit of the following:
    
    commit 6bc7ae65224aefa9cb4032e4433b78310101e61e
    Author: Logan Sandar <mlsandar@uw.edu>
    Date:   Thu Jun 18 10:22:08 2020 -0700
    
        add constraints to db
    
    commit fa73a2c19e3757d4fd6a8a394ed8c66b329805a1
    Merge: 820e31d2 90eefabb
    Author: Logan Sandar <mlsandar@uw.edu>
    Date:   Thu Jun 11 10:34:04 2020 -0700
    
        Merge branch 'release/guppy' of ssh://stash.ihme.washington.edu:7999/cc/jobmon into release/guppy
    
    commit 820e31d2897c3c327dce471e560c99419a535480
    Author: Logan Sandar <mlsandar@uw.edu>
    Date:   Tue May 5 10:54:50 2020 -0700
    
        get mproc working
    
    commit 4dc58231b52bf73b6d7047c22f56de74d608d041
    Merge: 0b5160a1 49e0ba76
    Author: Logan Sandar <mlsandar@uw.edu>
    Date:   Tue May 5 08:02:42 2020 -0700
    
        Merge branch 'mproc' of ssh://stash.ihme.washington.edu:7999/~mlsandar/jobmon into release/guppy
    
    commit 0b5160a14f612ef88678fb40b6470f64d0da0bd1
    Merge: 9d5d837b 8d9e7362
    Author: Logan Sandar <mlsandar@uw.edu>
    Date:   Tue May 5 08:02:09 2020 -0700
    
        Merge branch 'release/guppy' of ssh://stash.ihme.washington.edu:7999/~mlsandar/jobmon into release/guppy
    
    commit 9d5d837b60ccb4a2c107c2f1ba2f2a9bc2d2bded
    Author: Logan Sandar <mlsandar@uw.edu>
    Date:   Tue May 5 08:01:00 2020 -0700
    
        more protections for bad SGE resource requests
    
    commit 49e0ba7617ca9ef7a51e7818cc30d89a1ba4ec91
    Author: Logan Sandar <mlsandar@uw.edu>
    Date:   Tue May 5 07:57:32 2020 -0700
    
        test version of mproc
    
    commit 8d9e7362d2b24aa8df88e551b521128367b7a21d
    Merge: b12a3ef8 b6748bde
    Author: Logan Sandar <mlsandar@uw.edu>
    Date:   Mon May 4 20:51:21 2020 +0000
    
        Merge pull request #9 in ~MLSANDAR/jobmon from merge to release/guppy
    
        * commit 'b6748bdeab39257c44dfad9eb71a16f52525de0d':
          resolve merge conflicts
          update gitignore file
          refactor for executor parameters done
    
    commit b6748bdeab39257c44dfad9eb71a16f52525de0d
    Author: Logan Sandar <mlsandar@uw.edu>
    Date:   Mon May 4 13:49:00 2020 -0700
    
        resolve merge conflicts
    
    commit f17fe41e01c0351a4e37ce45d810724f9b54d910
    Author: Logan Sandar <mlsandar@uw.edu>
    Date:   Fri May 1 12:17:06 2020 -0700
    
        fix last 2 broken tests
    
    commit 676133e08bcc566c3c9c17bb2fb2586d9fb646da
    Merge: 60615576 154aab4d
    Author: Logan Sandar <mlsandar@uw.edu>
    Date:   Thu Apr 30 09:48:10 2020 -0700
    
        resolve conflicts
    
    commit 154aab4d233fa7ed659deed731b18d10447a09f0
    Author: everdyke <everdyke@uw.edu>
    Date:   Thu Apr 30 09:27:41 2020 -0700
    
        Some testing fixes
    
    commit 60615576c9e3cc2ef75865d46dfbf19f80900add
    Merge: f3615a7b 500607fc
    Author: Logan Sandar <mlsandar@uw.edu>
    Date:   Thu Mar 26 10:09:50 2020 -0700
    
        fix merge conflicts
    
    commit f3615a7b99fbfcea2c8e82ab371e6d49273a7fb4
    Merge: 331a5233 b3bcc685
    Author: Logan Sandar <mlsandar@uw.edu>
    Date:   Mon Mar 16 14:57:50 2020 -0700
    
        resolve merge conflicts
    
    commit 331a52334de0e019bd639c26227416ec0aed95f6
    Author: Logan Sandar <mlsandar@uw.edu>
    Date:   Mon Mar 16 12:15:49 2020 -0700
    
        update gitignore file
    
    commit 09cdfc51f3efce22d3943d062a4f926d8bd63486
    Merge: a83ca721 16d8d8c2
    Author: Logan Sandar <mlsandar@uw.edu>
    Date:   Fri Mar 13 16:02:12 2020 -0700
    
        Merge branch 'release/guppy' of ssh://stash.ihme.washington.edu:7999/~mlsandar/jobmon into release/guppy
    
    commit a83ca7211c531a523b34a0b7d54951e63a08e066
    Author: Logan Sandar <mlsandar@uw.edu>
    Date:   Fri Mar 13 16:01:52 2020 -0700
    
        refactor for executor parameters done

--- jobmon/server/deployment/container/db/table006-task_template_version.sql
+++ jobmon/server/deployment/container/db/table006-task_template_version.sql
@@ -9,10 +9,11 @@ DROP TABLE IF EXISTS `task_template_version`;
 CREATE TABLE `task_template_version`(
   `id` INTEGER NOT NULL AUTO_INCREMENT,
   `task_template_id` INTEGER NOT NULL,
-  `command_template` text NOT NULL,
   `arg_mapping_hash` VARCHAR(150) NOT NULL,
+  `command_template` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- add index on our fake foreign key
 ALTER TABLE `task_template_version` ADD INDEX `ix_task_template_id` (`task_template_id`);
+ALTER TABLE `task_template_version` ADD CONSTRAINT `uc_composite_pk` UNIQUE (`task_template_id`, `arg_mapping_hash`, `command_template`);
