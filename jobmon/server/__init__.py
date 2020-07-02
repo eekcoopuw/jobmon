e0420b47b6e93ce09ec3fe6d8b599264f2b61379
Logan Sandar <mlsandar@uw.edu>
2020-07-02 11:45:03 -0700

    try and update logging in server

--- jobmon/server/__init__.py
+++ jobmon/server/__init__.py
@@ -1,3 +1,6 @@
+import sys
+import logging
+
 from flask import Flask
 from flask_cors import CORS
 
@@ -17,6 +20,8 @@ def create_app(config=None):
     from jobmon.server.update_server.update_server import jsm
     from jobmon.server.visualization_server.visualization_server \
         import jvs
+
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
     app = Flask(__name__)
     if config is None:
