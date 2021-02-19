0b683ae1ce3e06ac9f7e4ec85dd5d53b101eb417
limingxu <limingxu@uw.edu>
2021-02-19 14:54:11 -0800

    Add parameters

--- deployment/tests/three_phase_load_test.py
+++ deployment/tests/three_phase_load_test.py
@@ -17,7 +17,7 @@ thisdir = os.path.dirname(os.path.realpath(os.path.expanduser(__file__)))
 random.seed(12345)
 
 
-def three_phase_load_test(n_jobs: int, wfid: str = "") -> None:
+def three_phase_load_test(n_tasks: int, wfid: str = "") -> None:
     """
     Creates and runs one workflow with n jobs and another 3n jobs that have
     dependencies to the previous n jobsm then a final tier that has n jobs
@@ -50,7 +50,7 @@ def three_phase_load_test(n_jobs: int, wfid: str = "") -> None:
     tier1 = []
     counter = 0
     # First Tier
-    for i in range(n_jobs):
+    for i in range(n_tasks):
         counter += 1
         sleep_time = random.randint(30, 41)
         # 3  new attributes per task with values to cause full new bind for every attribute
@@ -68,7 +68,7 @@ def three_phase_load_test(n_jobs: int, wfid: str = "") -> None:
 
     tier2 = []
     # Second Tier, depend on 1 tier 1 task
-    for i in range(n_jobs * 3):
+    for i in range(n_tasks * 3):
         counter += 1
         sleep_time = random.randint(30, 41)
         # Same 3 attributes for every tier 2 task
@@ -80,13 +80,13 @@ def three_phase_load_test(n_jobs: int, wfid: str = "") -> None:
             counter=counter,
             wfid=wfid,
             task_attributes=attributes_t2,
-            upstream_tasks=[tier1[(i % n_jobs)]]
+            upstream_tasks=[tier1[(i % n_tasks)]]
         )
         tier2.append(tier_2_task)
 
     tier3 = []
     # Third Tier, depend on 3 tier 2 tasks
-    for i in range(n_jobs):
+    for i in range(n_tasks):
         counter += 1
         sleep_time = random.randint(30, 41)
         # each task has 30-40 attributes with no value assigned yet
@@ -100,13 +100,13 @@ def three_phase_load_test(n_jobs: int, wfid: str = "") -> None:
             counter=counter,
             wfid=wfid,
             task_attributes=attributes_t3,
-            upstream_tasks=[tier2[i], tier2[(i + n_jobs)], tier2[(i + (2 * n_jobs))]]
+            upstream_tasks=[tier2[i], tier2[(i + n_tasks)], tier2[(i + (2 * n_tasks))]]
         )
         tier3.append(tier_3_task)
 
     wf.add_tasks(tier1 + tier2 + tier3)
 
-    num_tasks = 5 * n_jobs
+    num_tasks = 5 * n_tasks
     time = datetime.now().strftime("%m/%d/%Y_%H:%M:%S")
     print(f"{time}: Beginning the workflow, there are {num_tasks} tasks in "
           f"this DAG")
@@ -120,7 +120,10 @@ def parse_arguments(argstr: Optional[str] = None) -> Namespace:
     Gets arguments from the command line or a command line string.
     """
     parser = ArgumentParser()
-    parser.add_argument("--num", type=int, required=True, default=1)
+    parser.add_argument("--numTask", type=int, required=True, default=1)
+    parser.add_argument("--numTaskArg", type=int, required=True, default=0)
+    parser.add_argument("--comLength", type=int, required=True, default=0)
+    parser.add_argument("--numEdge", type=int, required=True, default=0)
     parser.add_argument("--wfid", type=str, required=True)
 
     if argstr is not None:
@@ -134,4 +137,5 @@ def parse_arguments(argstr: Optional[str] = None) -> Namespace:
 
 if __name__ == "__main__":
     args = parse_arguments()
-    three_phase_load_test(n_jobs=args.num, wfid=args.wfid)
+    three_phase_load_test(n_tasks=args.numTask, wfid=args.wfid, n_taskargs=args.numTaskArg,
+                          l_com=args.comLength, n_edge=args.nuEdge)
