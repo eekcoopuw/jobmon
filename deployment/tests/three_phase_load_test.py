6c9d6e0d5c82a01852b97a1642bc66a4d8820e79
limingxu <limingxu@uw.edu>
2021-02-22 14:22:39 -0800

    Tested

--- deployment/tests/three_phase_load_test.py
+++ deployment/tests/three_phase_load_test.py
@@ -17,13 +17,23 @@ thisdir = os.path.dirname(os.path.realpath(os.path.expanduser(__file__)))
 random.seed(12345)
 
 
-def three_phase_load_test(n_tasks: int, wfid: str = "") -> None:
+def three_phase_load_test(n_tasks: int, wfid: str = "", n_taskargs: int = 1, l_com: int = 0, n_edges: int = 0) -> None:
     """
-    Creates and runs one workflow with n jobs and another 3n jobs that have
-    dependencies to the previous n jobsm then a final tier that has n jobs
+    Creates and runs one workflow with n_tasks tasks of n_taskargs args
+    and length of l_com command and another n_edge jobs that have
+    dependencies to the previous n_tasks jobsm then a final tier that has n_tasks jobs
     with multiple dependencies on the 3n job tier. This can test how jobmon
     will respond when there is a large load of connections and communication
     to and from the db (like when dismod or codcorrect run)
+
+                t1-1     t1-2       ...   t1-n_tasks
+                 |        |         ...
+                t2-1     t2-2       ...   t2-n_edges
+                 |\      /|\                /|
+                 | \    / | \       ...    / |
+                 ...
+                 |    /  \|/        ...    \ |
+                t3-1     t3-2              t3-n_tasks
     """
     unknown_tool = Tool()
 
@@ -33,9 +43,10 @@ def three_phase_load_test(n_tasks: int, wfid: str = "") -> None:
     os.chmod(command, st.st_mode | stat.S_IXUSR)
     tt = unknown_tool.get_task_template(
         template_name="sleep_and_echo",
-        command_template="{thisdir}/sleep_and_echo.sh {sleep_time} {counter} {wfid}",
-        node_args=["sleep_time", "counter"],
-        task_args=["wfid"],
+        command_template="{thisdir}/sleep_and_echo.sh {sleep_time} {counter} {filler} " + \
+                         str(["{wfid" + str(i) + "}" for i in range(3)])[1:-1].replace(",", "").replace("\'", ""),
+        node_args=["sleep_time", "counter", "filler"],
+        task_args=["wfid" + str(k) for k in range(n_taskargs)],
         op_args=["thisdir"]
     )
 
@@ -49,6 +60,8 @@ def three_phase_load_test(n_tasks: int, wfid: str = "") -> None:
 
     tier1 = []
     counter = 0
+    # Make command long enough
+    filler = "x" * l_com
     # First Tier
     for i in range(n_tasks):
         counter += 1
@@ -61,14 +74,15 @@ def three_phase_load_test(n_tasks: int, wfid: str = "") -> None:
             thisdir=thisdir,
             sleep_time=sleep_time,
             counter=counter,
-            wfid=wfid,
-            task_attributes=attributes_t1
+            filler=filler,
+            task_attributes=attributes_t1,
+            **{"wfid" + str(k): k for k in range(n_taskargs)}
         )
         tier1.append(tier_1_task)
 
     tier2 = []
     # Second Tier, depend on 1 tier 1 task
-    for i in range(n_tasks * 3):
+    for i in range(n_edges):
         counter += 1
         sleep_time = random.randint(30, 41)
         # Same 3 attributes for every tier 2 task
@@ -78,14 +92,15 @@ def three_phase_load_test(n_tasks: int, wfid: str = "") -> None:
             thisdir=thisdir,
             sleep_time=sleep_time,
             counter=counter,
-            wfid=wfid,
+            filler=filler,
             task_attributes=attributes_t2,
-            upstream_tasks=[tier1[(i % n_tasks)]]
+            upstream_tasks=[tier1[(i % n_tasks)]],
+            **{"wfid" + str(k): k for k in range(n_taskargs)}
         )
         tier2.append(tier_2_task)
 
     tier3 = []
-    # Third Tier, depend on 3 tier 2 tasks
+    # Third Tier, depend on all tier 2 tasks
     for i in range(n_tasks):
         counter += 1
         sleep_time = random.randint(30, 41)
@@ -98,15 +113,16 @@ def three_phase_load_test(n_tasks: int, wfid: str = "") -> None:
             thisdir=thisdir,
             sleep_time=sleep_time,
             counter=counter,
-            wfid=wfid,
+            filler=filler,
             task_attributes=attributes_t3,
-            upstream_tasks=[tier2[i], tier2[(i + n_tasks)], tier2[(i + (2 * n_tasks))]]
+            upstream_tasks=tier2,
+            **{"wfid" + str(k): k for k in range(n_taskargs)}
         )
         tier3.append(tier_3_task)
 
     wf.add_tasks(tier1 + tier2 + tier3)
 
-    num_tasks = 5 * n_tasks
+    num_tasks = len(tier1) + len(tier2) + len(tier3)
     time = datetime.now().strftime("%m/%d/%Y_%H:%M:%S")
     print(f"{time}: Beginning the workflow, there are {num_tasks} tasks in "
           f"this DAG")
@@ -138,4 +154,4 @@ def parse_arguments(argstr: Optional[str] = None) -> Namespace:
 if __name__ == "__main__":
     args = parse_arguments()
     three_phase_load_test(n_tasks=args.numTask, wfid=args.wfid, n_taskargs=args.numTaskArg,
-                          l_com=args.comLength, n_edge=args.nuEdge)
+                          l_com=args.comLength, n_edges=args.numEdge)
