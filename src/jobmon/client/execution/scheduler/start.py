15e35515731d22797edd70d33d89b86ec097f818
Logan Sandar <mlsandar@uw.edu>
2021-03-05 16:26:59 -0800

    address linting

--- src/jobmon/client/execution/scheduler/start.py
+++ src/jobmon/client/execution/scheduler/start.py
@@ -4,7 +4,6 @@ from jobmon.client.execution.strategies.api import get_scheduling_executor_by_na
 from jobmon.client.execution.strategies.base import Executor
 from jobmon.client.execution.scheduler.task_instance_scheduler import TaskInstanceScheduler
 from jobmon.client.execution.scheduler.scheduler_config import SchedulerConfig
-from jobmon.log_config import configure_logger, get_rsyslog_handler_config
 from jobmon.requester import Requester
 
 
@@ -15,15 +14,6 @@ def get_scheduler(workflow_id: int, workflow_run_id: int,
                   *args, **kwargs) -> TaskInstanceScheduler:
     if scheduler_config is None:
         scheduler_config = SchedulerConfig.from_defaults()
-    if scheduler_config.use_rsyslog:
-        syslog_config = get_rsyslog_handler_config(
-            rsyslog_host=scheduler_config.rsyslog_host,
-            rsyslog_port=scheduler_config.rsyslog_port,
-            rsyslog_protocol=scheduler_config.rsyslog_protocol
-        )
-    else:
-        syslog_config = None
-    # configure_logger("jobmon.client.execution", syslog_config)
 
     # TODO: make the default executor configurable
     if executor is None:
