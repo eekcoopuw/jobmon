fb715b60ff10a9fb50af85ebd8e72ed30752ee2f
David Shaw <dhs2018@Davids-MacBook-Pro.local>
2021-03-09 17:10:36 -0800

    remove sleep from deploy script

--- ci/deploy_utils.sh
+++ ci/deploy_utils.sh
@@ -170,7 +170,6 @@ deploy_jobmon_to_k8s () {
             ${KUBECTL_CONTAINER} \
                 --namespace="$K8S_NAMESPACE" \
                 apply -f /data/${TEMPLATE}
-        sleep 10 # Rest for 10 seconds to let the pods calm down a bit
     done
 }
 
