46130f04e7b7763140b96c0367b27dc2e53023ff
limingxu <limingxu@uw.edu>
2021-05-25 11:29:26 -0700

    Unit tested

--- ci/share_conda_install.sh
+++ ci/share_conda_install.sh
@@ -5,14 +5,15 @@ env_base=$1
 jobmon_version=$2
 conda_root=$3
 
-env_name=$env_base/$jobmon_version
-minor_version=$(echo $jobmon_version |awk -F"." '{print $1"."$2}')
+minor_version=$(echo $jobmon_version |awk -F"." '{print $1"-"$2}')
+env_name=$env_base/jobmon_$(echo $jobmon_version | tr "." "-")
+
 export PATH=$PATH:$conda_root/bin
 eval "$(conda shell.bash hook)"
 
 echo "Creating environment $env_name"
 umask 002
-conda create -y --name="$env_name" python=3.9
+conda create -y --prefix="$env_name" python=3.9
 # check status of previous command
 if [ $? -eq 0 ]; then
     echo "Created environment ${env_name}"
@@ -31,6 +32,6 @@ else
 fi
 echo "Installing jobmon for environment $env_name" &&
 pip install jobmon==$jobmon_version &&
-echo "linking jobmon $env_name to  && $minor_version"
-eval "$(rm $env_base/$minor_version ||true)" &&
-eval "$(ln -s $env_base/$jobmon_version $env_base/$minor_version)
+echo "linking jobmon $env_name to  jobmon_$minor_version"
+eval "$(rm -rf $env_base/jobmon_$minor_version 2>/dev/null ||true)" # silent
+eval "$(ln -s $env_name $env_base/jobmon_$minor_version)"
