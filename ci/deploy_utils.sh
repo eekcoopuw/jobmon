db54f5a22ad7bb896d896bb0e56ad8cefbf88f06
Sam Hu <samhu@gen-uge-submit-p01.hosts.ihme.washington.edu>
2021-09-07 09:10:30 -0700

    python3.7 to python3.8 for six_tests

--- ci/deploy_utils.sh
+++ ci/deploy_utils.sh
@@ -207,7 +207,7 @@ test_k8s_deployment () {
 
     CONDA_DIR=$WORKSPACE/.conda_env/load_test
     $QLOGIN_ACTIVATE && \
-        conda create --prefix $CONDA_DIR python==3.7
+        conda create --prefix $CONDA_DIR python==3.8
     $QLOGIN_ACTIVATE &&
        conda activate $CONDA_DIR && \
        pip install jobmon==$JOBMON_VERSION && \
