e5e23f42e737a70263406945665f9fe233209759
Sam Hu <samhu@gen-slurm-slogin-s01.hosts.ihme.washington.edu>
2021-09-08 20:15:56 +0000

    update_config

--- ci/deploy_utils.sh
+++ ci/deploy_utils.sh
@@ -239,9 +239,9 @@ test_k8s_slurm_deployment () {
     pip install jobmon==$JOBMON_VERSION
     pip install jobmon_uge
     pip install jobmon_slurm
-    jobmon update_config --web_service_fqdn $TARGET_IP --web_service_port 80
+    /opt/slurm/bin/srun -n 1 -p all.q -A general -c 1 --mem=300 --time=100 jobmon update_config --web_service_fqdn $TARGET_IP --web_service_port 80
     # "/opt/slurm/bin/srun -n 1 -p all.q -A general -c 1 --mem=300 --time=100 python $WORKSPACE/deployment/tests/slurm/six_job_test.py"
     #python $WORKSPACE/deployment/tests/slurm/six_job_test.py
     # /opt/slurm/bin/srun -n 1 -p all.q -A general -c 1 --mem=300 --time=100 python $WORKSPACE/deployment/tests/slurm/six_job_test.py
-    /opt/slurm/bin/srun -n 1 -p all.q -A general -c 1 --mem=300 --time=100 python --version
+    /opt/slurm/bin/srun -n 1 -p all.q -A general -c 1 --mem=300 --time=100 python $WORKSPACE/deployment/tests/slurm/six_job_test.py
 }
