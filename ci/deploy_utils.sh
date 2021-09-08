b065992453d6d2c3d6145823f686e4089d1d49fb
Sam Hu <samhu@gen-slurm-slogin-s01.hosts.ihme.washington.edu>
2021-09-08 20:01:33 +0000

    with conda deactivate

--- ci/deploy_utils.sh
+++ ci/deploy_utils.sh
@@ -227,6 +227,7 @@ test_k8s_slurm_deployment () {
     TARGET_IP=$4
 
     conda info --envs
+    conda deactivate
     conda env remove --prefix $CONDA_DIR_SLURM python==3.8
     conda info --envs
     CONDA_DIR=$WORKSPACE/.conda_env/load_test_slurm
