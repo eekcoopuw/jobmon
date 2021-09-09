679fbf39fdbd04764b4fcec664ae46b40971bded
Sam Hu <samhu@gen-slurm-slogin-s01.hosts.ihme.washington.edu>
2021-09-09 20:53:14 +0000

    conda update conda

--- ci/deploy_utils.sh
+++ ci/deploy_utils.sh
@@ -231,7 +231,7 @@ test_k8s_slurm_deployment () {
 
     conda info --envs
     conda deactivate
-    conda update -n base -c defaults conda
+    conda update conda
     conda env remove --prefix $CONDA_DIR_SLURM python==3.8
     conda info --envs
     CONDA_DIR_SLURM=$WORKSPACE/.conda_env/load_test_slurm
