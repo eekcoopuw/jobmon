dcca81e5e2ad6f8836c05c03a1ee3c61f78735b2
Sam Hu <samhu@gen-slurm-slogin-s01.hosts.ihme.washington.edu>
2021-09-10 03:19:35 +0000

    the slurm actually gets called

--- ci/deploy_utils.sh
+++ ci/deploy_utils.sh
@@ -242,39 +242,11 @@ test_k8s_slurm_deployment () {
     conda info --envs
     pip install pyyaml
     pip install jobmon==$JOBMON_VERSION && \
-    var=$(jobmon update_config)
-    echo "var: $var"
     pip install slurm_rest && \
-    var=$(jobmon update_config)
-    echo "var: $var"
     pip install jobmon_uge && \
-    var=$(jobmon update_config)
-    echo "var: $var"
     pip install jobmon_slurm && \
-    var=$(jobmon update_config)
-    echo "var: $var"
     pip freeze && \
-    var=$(jobmon update_config)
-    echo "var: $var"
     jobmon update_config --web_service_fqdn $TARGET_IP --web_service_port 80
     srun -n 1 -p all.q -A general -c 1 --mem=300 --time=100 python $WORKSPACE/deployment/tests/slurm/six_job_test.py
 
-#    source /homes/svcscicompci/miniconda3/bin/activate base && \
-#      conda info --envs && \
-#      conda deactivate && \
-#      conda env remove --prefix $CONDA_DIR_SLURM python==3.8 && \
-#      conda info --envs && \
-#      CONDA_DIR_SLURM=$WORKSPACE/.conda_env/load_test_slurm && \
-#      conda create --prefix $CONDA_DIR_SLURM python==3.8 && \
-#      conda activate $CONDA_DIR_SLURM && \
-#      conda info --envs && \
-#      pip install pyyaml && \
-#      pip install jobmon==$JOBMON_VERSION && \
-#      pip install slurm_rest && \
-#      pip install jobmon_uge && \
-#      pip install jobmon_slurm && \
-#      PATH=$PATH:/opt/slurm/bin && \
-#      pip freeze && \
-#      jobmon update_config --web_service_fqdn $TARGET_IP --web_service_port 80 && \
-#      srun -n 1 -p all.q -A general -c 1 --mem=300 --time=100 python $WORKSPACE/deployment/tests/slurm/six_job_test.py
 }
