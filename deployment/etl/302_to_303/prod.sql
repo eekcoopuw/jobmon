c090028346a2b03aa154c5db202dbb9e9a440fc6
Sam Hu <samhu@int-uge-archive-p007.cluster.ihme.washington.edu>
2021-12-29 07:53:56 -0800

    commit schema data updates for 302_to_303

---
+++ deployment/etl/302_to_303/prod.sql
@@ -0,0 +1,17 @@
+-- Due to edge volume,
+-- Set timeout setting first: Edit -> SQL Editor -> DBMS connection read timeout interval + DBMS connection timeout interval 6000
+USE docker;
+
+ALTER TABLE cluster CHANGE connection_string connection_parameters VARCHAR(2500);
+
+UPDATE cluster
+SET connection_parameters = '{"slurm_rest_host": "https://api.cluster.ihme.washington.edu"}'
+WHERE id IN (
+	SELECT c.id
+    FROM cluster c
+		INNER JOIN cluster_type ct ON c.cluster_type_id = ct.id
+        WHERE ct.name = 'slurm');
+
+ALTER TABLE edge
+	CHANGE upstream_node_ids upstream_node_ids LONGTEXT,
+	CHANGE downstream_node_ids downstream_node_ids LONGTEXT;
