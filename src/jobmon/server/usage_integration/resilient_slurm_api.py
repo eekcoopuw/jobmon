75758e72be11dfeee2104e2da3f9e316c006bdbb
David Shaw <dhs2018@Davids-MacBook-Pro.local>
2022-02-24 16:17:30 -0800

    final sketch of integrator refactor

--- src/jobmon/server/usage_integration/resilient_slurm_api.py
+++ src/jobmon/server/usage_integration/resilient_slurm_api.py
@@ -1,9 +1,9 @@
-from typing import Any
+import time
+from typing import Any, List
 
 from slurm_rest import ApiClient  # type: ignore
 from slurm_rest.api import SlurmApi  # type: ignore
 from slurm_rest.rest import ApiException  # type: ignore
-from slurm_rest.models import V0036JobSubmission  # type: ignore  # noqa: I100
 from tenacity import retry, retry_if_exception_type, stop_after_delay, wait_exponential
 
 
@@ -13,36 +13,15 @@ class ResilientSlurmApi(SlurmApi):
     All routes will be retried for
     """
 
+    # Treat this class like a singleton
+    instance = None
+    last_token_refresh_date = 0
+
     def __init__(self, api_client: ApiClient) -> None:
         """Initialize the API object."""
         super().__init__(api_client)
-
-    @retry(
-        retry=retry_if_exception_type(ApiException),
-        wait=wait_exponential(max=90),
-        stop=stop_after_delay(180),
-    )
-    def slurmctld_get_jobs(self) -> Any:
-        """Get running job IDs from the rest API."""
-        return super().slurmctld_get_jobs()
-
-    @retry(
-        retry=retry_if_exception_type(ApiException),
-        wait=wait_exponential(max=90),
-        stop=stop_after_delay(180),
-    )
-    def slurmctl_submit_job(self, job_submission: V0036JobSubmission) -> Any:
-        """Submit a batch job to the scheduler."""
-        return super().slurmctld_submit_job(job_submission)
-
-    @retry(
-        retry=retry_if_exception_type(ApiException),
-        wait=wait_exponential(max=90),
-        stop=stop_after_delay(180),
-    )
-    def slurmctld_cancel_job(self, job_id: int) -> Any:
-        """Cancel jobs by ID."""
-        return super().slurmctld_cancel_job(job_id)
+        ResilientSlurmApi.instance = self
+        ResilientSlurmApi.last_token_refresh_date = time.time()
 
     @retry(
         retry=retry_if_exception_type(ApiException),
