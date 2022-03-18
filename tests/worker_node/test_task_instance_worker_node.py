610abb1536b3a3d257b4a8ee190766fe06d86fea
Logan Sandar <mlsandar@uw.edu>
2022-03-18 11:36:57 -0700

    worker node tests are working

--- tests/worker_node/test_task_instance_worker_node.py
+++ tests/worker_node/test_task_instance_worker_node.py
@@ -1,220 +1,200 @@
 import os
 import random
+from typing import Dict
 
-import pytest
+from unittest.mock import patch
 
-from jobmon import __version__
-from jobmon.requester import Requester
 from jobmon.constants import TaskInstanceStatus
+from jobmon.client.distributor.distributor_service import DistributorService
+from jobmon.client.swarm.workflow_run import WorkflowRun as SwarmWorkflowRun
+from jobmon.cluster_type.dummy import DummyDistributor
+from jobmon.cluster_type.multiprocess.multiproc_distributor import MultiprocessDistributor
+from jobmon.cluster_type.sequential.seq_distributor import SequentialDistributor
+from jobmon.worker_node.worker_node_task_instance import WorkerNodeTaskInstance
 
 
-thisdir = os.path.dirname(os.path.realpath(os.path.expanduser(__file__)))
+class DoNothingDistributor(DummyDistributor):
+    def submit_to_batch_distributor(
+        self, command: str, name: str, requested_resources
+    ) -> str:
+        distributor_id = random.randint(1, int(1e7))
+        return str(distributor_id)
 
 
-def test_seq_kill_self_state():
-    """
-    mock the error status
-    """
-    from jobmon.cluster_type.sequential.seq_distributor import SequentialDistributor
+class DoNothingArrayDistributor(MultiprocessDistributor):
 
-    expected_words = "job was in kill self state"
-    executor = SequentialDistributor()
-    executor._exit_info = {1: 199}
-    r_value, r_msg = executor.get_remote_exit_info(1)
-    assert r_value == TaskInstanceStatus.UNKNOWN_ERROR
-    assert expected_words in r_msg
+    def submit_array_to_batch_distributor(
+        self, command: str, name: str, requested_resources, array_length: int,
+    ) -> Dict[int, str]:
+        job_id = random.randint(1, int(1e7))
+        mapping: Dict[int, str] = {}
+        for array_step_id in range(0, array_length):
+            distributor_id = self._get_subtask_id(job_id, array_step_id)
+            mapping[array_step_id] = distributor_id
+
+        return mapping
 
 
-@pytest.mark.parametrize(
-    "ti_state", [TaskInstanceStatus.UNKNOWN_ERROR, TaskInstanceStatus.KILL_SELF]
-)
-def test_ti_kill_self_state(db_cfg, tool, ti_state):
+def test_task_instance(db_cfg, tool):
     """should try to log a report by date after being set to the U or K state
     and fail"""
     from jobmon.client.swarm.workflow_run import WorkflowRun as SwarmWorkflowRun
     from jobmon.client.distributor.distributor_service import DistributorService
     from jobmon.worker_node.worker_node_task_instance import WorkerNodeTaskInstance
-    from jobmon.cluster_type.dummy import DummyDistributor
 
-    class DoNothingDistributor(DummyDistributor):
-        def submit_to_batch_distributor(
-            self, command: str, name: str, requested_resources
-        ) -> str:
-            distributor_id = random.randint(1, int(1e7))
-            return str(distributor_id)
-
-    workflow = tool.create_workflow(name=f"test_ti_kill_self_state_{ti_state}")
-    task_a = tool.active_task_templates["simple_template"].create_task(arg="sleep 120")
+    workflow = tool.create_workflow(name="test_ti_kill_self_state")
+    task_a = tool.active_task_templates["simple_template"].create_task(arg="echo 1")
     workflow.add_task(task_a)
-
-    # bind workflow to db
     workflow.bind()
     wfr = workflow._create_workflow_run()
 
-    # move workflow to launched state
-    distributor = DoNothingDistributor()
-    distributor_service = DistributorService(
-        workflow.workflow_id,
-        wfr.workflow_run_id,
-        distributor,
-        requester=workflow.requester,
-    )
-
-    # swarm calls
+    # create task instances
     swarm = SwarmWorkflowRun(
-        workflow_id=workflow.workflow_id,
         workflow_run_id=wfr.workflow_run_id,
-        tasks=list(workflow.tasks.values()),
-        requester=workflow.requester,
+        requester=workflow.requester
     )
-    swarm.compute_initial_dag_state()
-    list(swarm.queue_tasks())
+    swarm.from_workflow(workflow)
+    swarm.process_commands()
 
-    # launch task on executor
-    distributor_service.distributor.start()
-    distributor_task = distributor_service._get_tasks_queued_for_instantiation()[0]
-    distributor_task_instance = distributor_service._create_task_instance(
-        distributor_task
+    # test that we can launch via the normal job pathway
+    distributor_service = DistributorService(
+        DoNothingDistributor(),
+        requester=workflow.requester,
+        raise_on_error=True
     )
+    distributor_service.set_workflow_run(wfr.workflow_run_id)
+    distributor_service.process_status(TaskInstanceStatus.QUEUED)
+    distributor_service.process_status(TaskInstanceStatus.INSTANTIATED)
 
     # Bring in the worker node here since dummy executor is never run
+    task_instance_id = (
+        distributor_service._get_task(task_a.task_id).task_instance.task_instance_id
+    )
     worker_node_task_instance = WorkerNodeTaskInstance(
-        task_instance_id=distributor_task_instance.task_instance_id,
-        expected_jobmon_version=__version__,
-        cluster_type_name=distributor.cluster_type_name,
+        task_instance_id=task_instance_id,
+        cluster_name=distributor_service.cluster.cluster_name
     )
-
-    # Log running
-    _, _, _ = worker_node_task_instance.log_running(1)
-
-    # set task to kill self state. next heartbeat will fail and cause death
-    app = db_cfg["app"]
-    DB = db_cfg["DB"]
-    with app.app_context():
-        DB.session.execute(
-            """
-            UPDATE task_instance
-            SET status = '{}'
-            WHERE task_instance.task_id = {}
-            """.format(
-                ti_state, task_a.task_id
-            )
-        )
-        DB.session.commit()
-
-    assert worker_node_task_instance.in_kill_self_state()
+    worker_node_task_instance.run()
+    assert worker_node_task_instance.status == TaskInstanceStatus.DONE
+    assert worker_node_task_instance.command_return_code == 0
 
 
-def test_array_task_instance(tool, db_cfg, client_env, array_template):
+def test_array_task_instance(tool, db_cfg, client_env, array_template, monkeypatch):
     """Tests that the worker node is compatible with array task instances."""
-
-    from jobmon.client.distributor.distributor_array import DistributorArray
-    from jobmon.client.distributor.distributor_workflow_run import (
-        DistributorWorkflowRun,
-    )
 
     array1 = array_template.create_array(
         arg=[1, 2, 3], cluster_name="sequential", compute_resources={"queue": "null.q"}
     )
-
     workflow = tool.create_workflow(name="test_array_ti_selection")
-
     workflow.add_array(array1)
     workflow.bind()
     wfr = workflow._create_workflow_run()
 
-    requester = Requester(client_env)
-
-    # Move all tasks to Q state
-    for tid in (t.task_id for t in array1.tasks.values()):
-        _, _ = requester._send_request(
-            app_route=f"/task/{tid}/queue", message={}, request_type="post"
-        )
-
-    # Register TIs
-    distributor_wfr = DistributorWorkflowRun(
-        workflow_id=workflow.workflow_id,
+    # create task instances
+    swarm = SwarmWorkflowRun(
         workflow_run_id=wfr.workflow_run_id,
-        requester=requester,
+        requester=workflow.requester
     )
-    dtis = distributor_wfr.get_queued_task_instances(1000)
+    swarm.from_workflow(workflow)
+    swarm.process_commands()
 
-    # Append on batch number
-    dist_array = DistributorArray(
-        array1.array_id, array1.task_resources.id, array1.compute_resources, requester
+    # test that we can launch via the normal job pathway
+    distributor_service = DistributorService(
+        DoNothingArrayDistributor(),
+        requester=workflow.requester,
+        raise_on_error=True
     )
-    dist_array.instantiated_array_task_instance_ids = [
-        dti.task_instance_id for dti in dtis
-    ]
-    assert dist_array.batch_number == 0
-    dist_array.add_batch_number_to_task_instances()
-    assert dist_array.batch_number == 1
+    distributor_service.set_workflow_run(wfr.workflow_run_id)
+    distributor_service.process_status(TaskInstanceStatus.QUEUED)
+    distributor_service.process_status(TaskInstanceStatus.INSTANTIATED)
 
-    # Call the array method for all tasks and ensure full coverage of task instance IDs
-    # Indices are offset by 1 since the clusters will submit using a 1:N range strategy
-    task_instance_ids = set()
-    for i in range(1, len(dtis) + 1):
-        _, resp = requester._send_request(
-            app_route=f"/get_array_task_instance_id/{array1.array_id}/0/{i}",
-            message={},
-            request_type="get",
-        )
-        assert resp["task_instance_id"] not in task_instance_ids
-        task_instance_ids.add(resp["task_instance_id"])
+    for ti in distributor_service._get_array(array1.array_id).task_instances:
+        job_id, step_id = ti.distributor_id.split(".")
+        monkeypatch.setenv("JOB_ID", job_id)
+        monkeypatch.setenv("ARRAY_STEP_ID", step_id)
 
-    assert task_instance_ids == set([dti.task_instance_id for dti in dtis])
+        batch_num = ti.array_batch.batch_number
 
-    # Test array worker node functionality
-    from jobmon import __version__
+        wnti = WorkerNodeTaskInstance(cluster_name="multiprocess", array_id=ti.array_id,
+                                      batch_number=batch_num)
+        wnti.run()
 
-    # Mock a value, doesn't matter. Worker node inherits from the environment.
-    os.environ["JOB_ID"] = "1"
-    w = WorkerNodeTaskInstance(
-        task_instance_id=None,
-        expected_jobmon_version=__version__,
-        cluster_type_name="sequential",
-        array_id=array1.array_id,
-        batch_number=0,
+        assert wnti.status == TaskInstanceStatus.DONE
+        assert wnti.command_return_code == 0
+
+
+def test_ti_kill_self_state(db_cfg, tool):
+    """should try to log a report by date after being set to the U or K state
+    and fail"""
+
+    workflow = tool.create_workflow(name="test_ti_kill_self_state")
+    task_a = tool.active_task_templates["simple_template"].create_task(arg="sleep 120")
+    workflow.add_task(task_a)
+    workflow.bind()
+    wfr = workflow._create_workflow_run()
+
+    # create task instances
+    swarm = SwarmWorkflowRun(
+        workflow_run_id=wfr.workflow_run_id,
+        requester=workflow.requester
+    )
+    swarm.from_workflow(workflow)
+    swarm.process_commands()
+
+    # test that we can launch via the normal job pathway
+    distributor_service = DistributorService(
+        DoNothingDistributor(),
+        requester=workflow.requester,
+        raise_on_error=True
+    )
+    distributor_service.set_workflow_run(wfr.workflow_run_id)
+    distributor_service.process_status(TaskInstanceStatus.QUEUED)
+    distributor_service.process_status(TaskInstanceStatus.INSTANTIATED)
+
+    # Bring in the worker node here since dummy executor is never run
+    task_instance_id = (
+        distributor_service._get_task(task_a.task_id).task_instance.task_instance_id
+    )
+    worker_node_task_instance = WorkerNodeTaskInstance(
+        task_instance_id=task_instance_id,
+        cluster_name=distributor_service.cluster.cluster_name,
+        heartbeat_interval=5
     )
 
-    # Check that task instance ID property set correctly
-    tid = w.task_instance_id
-    assert tid == min(task_instance_ids)
+    # Log running
+    worker_node_task_instance.log_running()
 
-    proc_return_code = w.run(heartbeat_interval=90, report_by_buffer=3.1)
-    assert proc_return_code == 0
+    # patch so log_running does nothing and then put in K state. we should fail into error
+    # error state
+    worker_node = "jobmon.worker_node."
+    WNTI = "worker_node_task_instance.WorkerNodeTaskInstance."
+    with patch(worker_node + WNTI + "log_running") as m_run:
+        m_run.return_value = None
 
-    app, DB = db_cfg["app"], db_cfg["DB"]
+        # set task to kill self state. next heartbeat will fail and cause death
+        app = db_cfg["app"]
+        DB = db_cfg["DB"]
+        with app.app_context():
+            DB.session.execute(
+                """
+                UPDATE task_instance
+                SET status = '{}'
+                WHERE task_instance.task_id = {}
+                """.format(
+                    TaskInstanceStatus.KILL_SELF, task_a.task_id
+                )
+            )
+            DB.session.commit()
 
-    with app.app_context():
-        q = f"""
-        SELECT status
-        FROM task_instance
-        WHERE id = {tid}"""
+        worker_node_task_instance.run()
 
-        task_instance = DB.session.execute(q).one()
-        DB.session.commit()
-        assert task_instance.status == "D"
-
-    # Instantiate a second worker node instance, should correspond to the second TID
-    w2 = WorkerNodeTaskInstance(
-        task_instance_id=None,
-        expected_jobmon_version=__version__,
-        cluster_type_name="sequential",
-        array_id=array1.array_id,
-        batch_number=0,
-    )
-    assert w2.executor.array_step_id == 2
-    assert w2.task_instance_id == sorted(list(task_instance_ids))[1]
+    assert worker_node_task_instance.status == TaskInstanceStatus.ERROR
 
 
-def test_limited_error_log(db_cfg, client_env):
-    from jobmon.client.tool import Tool
-    from jobmon.client.swarm.workflow_run import WorkflowRun as SwarmWorkflowRun
-    from jobmon.client.distributor.distributor_service import DistributorService
-    from jobmon.cluster_type.sequential.seq_distributor import SequentialDistributor
+def test_limited_error_log(tool, db_cfg):
 
-    tool = Tool()
+    thisdir = os.path.dirname(os.path.realpath(os.path.expanduser(__file__)))
+
     wf = tool.create_workflow(name="random_workflow")
     template = tool.get_task_template(
         template_name="some_template",
