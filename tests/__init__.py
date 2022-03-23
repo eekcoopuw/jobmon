d029cec09e1400c67e20b15a5d9930b4c0a7e8ac
Sam Hu <samhu@gen-slurm-sarchive-s01.cluster.ihme.washington.edu>
2022-03-23 10:20:59 -0700

    minor change on __init__

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,15 +1,19 @@
 import getpass
 import os
+import logging
 import time
 
 import pytest
 from unittest.mock import patch
 
 from jobmon.client.tool import Tool
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from jobmon.server.usage_integration.usage_integrator import UsageIntegrator
-from tests.usage_integration import TUsageIntegrator
 from jobmon.server.usage_integration.usage_queue import UsageQ
 from jobmon.server.usage_integration.usage_utils import QueuedTI
+
+logger = logging.getLogger(__name__)
 
 
 class TUsageIntegrator(UsageIntegrator):
@@ -26,7 +30,7 @@ class TUsageIntegrator(UsageIntegrator):
         # self.config = _get_config()
         conn_slurm_sdb_str = "mysql://{user}:{pw}@{host}:{port}/{db}".format(
             user="jobmon",
-            pw="aedohvupi+h5iegho4Haix8Eem0peebe",
+            pw="",
             host="gen-slurm-sdb-s02.cluster.ihme.washington.edu",
             port="3306",
             db="slurm_acct_db",
@@ -41,3 +45,6 @@ class TUsageIntegrator(UsageIntegrator):
         eng_slurm_sdb = create_engine(conn_slurm_sdb_str, pool_recycle=200)
         session_slurm_sdb = sessionmaker(bind=eng_slurm_sdb)
         self.session_slurm_sdb = session_slurm_sdb()
+
+        self.tres_types = self._get_tres_types()
+        logger.info(f"tres types = {self.tres_types}")
