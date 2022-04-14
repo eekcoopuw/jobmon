ad757231f105da178d7af45cf07ac41cbcb99fe0
Sam Hu <samhu@gen-slurm-sarchive-s01.cluster.ihme.washington.edu>
2022-04-14 11:23:51 -0700

    update func all params

--- tests/usage_integration/test_usage_integration.py
+++ tests/usage_integration/test_usage_integration.py
@@ -1,84 +1,16 @@
 import getpass
 import os
-import logging
 import time
 
 import pytest
 from unittest.mock import patch
 
 from jobmon.client.tool import Tool
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 # Commented out since slurm_rest is not a testing requirement
-from jobmon.server.usage_integration.usage_integrator import UsageIntegrator
+# from jobmon.server.usage_integration.usage_integrator import UsageIntegrator
 # from jobmon.server.usage_integration.usage_queue import UsageQ
-from jobmon.server.usage_integration.usage_utils import QueuedTI
+# from jobmon.server.usage_integration.usage_utils import QueuedTI
 
-logger = logging.getLogger(__name__)
-
-class TUsageIntegrator(UsageIntegrator):
-
-    def __init__(self) -> None:
-        self._connection_params = {}
-        self._slurm_api = None
-        self.heartbeat_time = 0
-        self.token_refresh_time = 0
-        self.token_lifespan = 86400  # TODO: make this configurable
-        self.user = "svcscicompci"
-
-        # Initialize config
-        # self.config = _get_config()
-        conn_slurm_sdb_str = "mysql://{user}:{pw}@{host}:{port}/{db}".format(
-            user="jobmon",
-            pw="Fill in pass",
-            host="gen-slurm-sdb-s02.cluster.ihme.washington.edu",
-            port="3306",
-            db="slurm_acct_db",
-        )
-
-        # # Initialize sqlalchemy session
-        # eng = create_engine(self.config["conn_str"], pool_recycle=200)
-        # session = sessionmaker(bind=eng)
-        # self.session = session()
-
-        # Initialize sqlalchemy session for slurm_sdb
-        eng_slurm_sdb = create_engine(conn_slurm_sdb_str, pool_recycle=200)
-        session_slurm_sdb = sessionmaker(bind=eng_slurm_sdb)
-        self.session_slurm_sdb = session_slurm_sdb()
-
-        self.tres_types = self._get_tres_types()
-        logger.info(f"tres types = {self.tres_types}")
-
-# @pytest.mark.skip(
-#     reason="This is a unit test, not an integration test."
-# )
-def test_get_slurm_resource_usages_via_slurm_sdb():
-    """This is to verify _get_squid_resource works.
-    """
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-    from jobmon.server.usage_integration.usage_queue import UsageQ
-    from jobmon.server.usage_integration.usage_utils import QueuedTI
-    from jobmon.server.usage_integration.usage_integrator import UsageIntegrator
-    from jobmon.server.usage_integration.usage_integrator import _get_squid_resource_via_slurm_sdb
-
-    usage_integrator = TUsageIntegrator()
-
-    distributor_ids = [201, 223, 25420, 595825, 595826, 595827]
-    qtis = [QueuedTI(
-        task_instance_id=distributor_id,  # fake task_instance_id
-        distributor_id=distributor_id,
-        cluster_type_name="slurm",
-        cluster_id=1,
-    ) for distributor_id in distributor_ids]
-
-    all_usage_stats = _get_squid_resource_via_slurm_sdb(
-        session=usage_integrator.session_slurm_sdb,
-        tres_types=usage_integrator.tres_types,
-        task_instances=qtis
-    )
-
-    print(f"all_usage_stats = {all_usage_stats}")
 
 @pytest.mark.skip(
     reason="This is not a regress test." "But useful to verify _get_squid_resource."
