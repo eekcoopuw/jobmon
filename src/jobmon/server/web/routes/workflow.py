d7f2370a9a47731be4efc8ff23339cb709e8dd36
gphipps <Guinop4>
2022-05-10 11:29:07 -0700

    Remove print

--- src/jobmon/server/web/routes/workflow.py
+++ src/jobmon/server/web/routes/workflow.py
@@ -327,7 +327,6 @@ def workflow_is_resumable(workflow_id: int) -> Any:
 @finite_state_machine.route("/workflow/<workflow_id>/get_max_concurrently_running", methods=["GET"])
 def get_max_concurrently_running(workflow_id: int) -> Any:
     """Return the maximum concurrency of this workflow"""
-    print(f"QQQQ Server workflow id {workflow_id}")
     bind_to_logger(workflow_id=workflow_id)
     query = """
         SELECT
