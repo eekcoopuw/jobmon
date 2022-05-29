eb3442961c061b3985efb01eb20e9488e0f9e428
Sam Hu <samhu@gen-slurm-sarchive-p0016.cluster.ihme.washington.edu>
2022-05-29 15:11:14 -0700

    real upload dev

--- ci/deploy_utils.sh
+++ ci/deploy_utils.sh
@@ -70,21 +70,21 @@ upload_python_dist () {
     $ACTIVATE && nox --session build
     PYPI_URL="https://artifactory.ihme.washington.edu/artifactory/api/pypi/pypi-shared"
     JOBMON_VERSION=$(basename $(find ./dist/jobmon-*.tar.gz) | sed "s/jobmon-\\(.*\\)\\.tar\\.gz/\\1/")
-    # if [[ "$JOBMON_VERSION" =~ "dev" ]]
-    # then
-    #  $ACTIVATE && twine upload \
-    #    --repository-url $PYPI_URL \
-    #    --username $REG_USERNAME \
-    #    --password $REG_PASSWORD \
-    #    --skip-existing \
-    #    ./dist/*
-    # else
-    #  $ACTIVATE && twine upload \
-    #    --repository-url $PYPI_URL \
-    #    --username $REG_USERNAME \
-    #    --password $REG_PASSWORD \
-    #    ./dist/*
-    # fi
+    if [[ "$JOBMON_VERSION" =~ "dev" ]]
+    then
+      $ACTIVATE && twine upload \
+        --repository-url $PYPI_URL \
+        --username $REG_USERNAME \
+        --password $REG_PASSWORD \
+        --skip-existing \
+        ./dist/*
+    else
+      $ACTIVATE && twine upload \
+        --repository-url $PYPI_URL \
+        --username $REG_USERNAME \
+        --password $REG_PASSWORD \
+        ./dist/*
+    fi
     echo "Jobmon v$JOBMON_VERSION deployed to Pypi"
 
 }
