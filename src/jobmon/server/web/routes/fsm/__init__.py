7a27d997638c483df038903bb2bb815ba7bc1855
Christina Everdyke <everdyke@uw.edu>
2022-08-12 09:37:12 -0400

    lint and typecheck passing

--- src/jobmon/server/web/routes/fsm/__init__.py
+++ src/jobmon/server/web/routes/fsm/__init__.py
@@ -10,7 +10,9 @@ blueprint = Blueprint("finite_state_machine", __name__)
 blueprint.add_url_rule("/", view_func=routes.is_alive, methods=["GET"])
 blueprint.add_url_rule("/time", view_func=routes.get_pst_now, methods=["GET"])
 blueprint.add_url_rule("/health", view_func=routes.health, methods=["GET"])
-blueprint.add_url_rule("/test_bad", view_func=routes.test_route, methods=["GET"])  # type: ignore
+blueprint.add_url_rule(
+    "/test_bad", view_func=routes.test_route, methods=["GET"]  # type: ignore
+)
 
 from jobmon.server.web.routes.fsm import (
     array,
