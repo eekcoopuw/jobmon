f1b235a45db9238bed336a7e7943911eacb63d04
limingxu <limingxu@uw.edu>
2022-11-09 11:16:05 -0800

    pin down pytest and add my github ticket to comments

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ SERVER_REQUIRES = [
 
 # pip install -e .[test]
 TEST_REQUIRES = [
-    "pytest",
+    "pytest==7.1.1",  #  https://github.com/pytest-dev/pytest/issues/10487
     "pytest-xdist",
     "pytest-cov",
     "mock",
