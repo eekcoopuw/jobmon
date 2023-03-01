54643bafa6603a342a71c8ad3180363c6506ed27
Christina Everdyke <everdyke@uw.edu>
2023-03-01 18:17:09 +0000

    Pull request #870: GBDSCI-5232 fix npm package errors
    Merge in SCIC/jobmon from GBDSCI-5232-fix-npm-package-errors to release/3.2
    
    Squashed commit of the following:
    
    commit 73e760d21d7577fb027000f60a334d5cf9eeea5d
    Author: Christina Everdyke <everdyke@uw.edu>
    Date:   Mon Feb 27 16:12:10 2023 -0800
    
        got npm install errors down to 2. Page is still loading as expected
    
    commit f186d0c894995d6cd97fedb8a18e627e055b4956
    Author: Christina Everdyke <everdyke@uw.edu>
    Date:   Mon Feb 27 16:00:52 2023 -0800
    
        Fixed some eslint errors. Down to 8 npm install errors, going to try to get it lower

--- jobmon_gui/src/task_details_page/node_list.tsx
+++ jobmon_gui/src/task_details_page/node_list.tsx
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from "react-router-dom";
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
-import { ListGroupItem } from 'react-bootstrap';
 
 
 export default function NodeLists({ upstreamTasks, downstreamTasks }) {
